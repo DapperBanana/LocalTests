6f2fbcd5b6509817486f93ed7d7ab4b9c2052b56
Austin L. Howard <austin.lane.howard@gmail.com>
2024-01-06 20:00:05 -0700

    Added support for user preferences

--- base_test.py
+++ base_test.py
@@ -1,14 +1,8 @@
 
-def reverse_matrix(matrix):
-    # Reverse the order of rows
-    matrix = matrix[::-1]
-    
-    # Reverse the order of columns
-    matrix = [row[::-1] for row in matrix]
-    
-    return matrix
+base1 = float(input("Enter the length of the first base: "))
+base2 = float(input("Enter the length of the second base: "))
+height = float(input("Enter the height of the trapezoid: "))
 
-# Example usage
-matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-reversed_matrix = reverse_matrix(matrix)
-print(reversed_matrix)
+area = 0.5 * (base1 + base2) * height
+
+print("The area of the trapezoid is:", area)
