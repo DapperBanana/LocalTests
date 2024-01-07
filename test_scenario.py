5a4f04a333caf516da3de4d7e1914e3fe4d57f99
Austin L. Howard <austin.lane.howard@gmail.com>
2024-01-06 20:58:59 -0700

    Integrated continuous integration workflow for automated testing

---
+++ test_scenario.py
@@ -0,0 +1,9 @@
+
+def sort_list_ascending(input_list):
+    sorted_list = sorted(input_list)
+    return sorted_list
+
+# Example usage
+num_list = [5, 2, 8, 3, 1]
+sorted_num_list = sort_list_ascending(num_list)
+print(sorted_num_list)
