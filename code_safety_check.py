ccbd9bd9f6bf1cff6272ab028acda11029745183
Austin L. Howard <austin.lane.howard@gmail.com>
2024-01-07 17:33:37 -0700

    Reformatted code for consistency

---
+++ code_safety_check.py
@@ -0,0 +1,8 @@
+
+def find_sum_of_list_elements(lst):
+    return sum(lst)
+
+# Example usage
+my_list = [1, 2, 3, 4, 5]
+sum_of_elements = find_sum_of_list_elements(my_list)
+print("Sum of elements:", sum_of_elements)
