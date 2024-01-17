480cac7964abd7a4a115e1d7a25df1b2fc0e5807
Austin L. Howard <austin.lane.howard@gmail.com>
2024-01-17 14:56:30 -0700

    Implemented lazy loading for images

--- preliminary_test.py
+++
@@ -1,7 +0,0 @@
-
-def reverse_string(text):
-    return text[::-1]
-
-input_string = input("Enter a string: ")
-reversed_string = reverse_string(input_string)
-print("Reversed string:", reversed_string)
