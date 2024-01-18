15ab71c19a64d1ab4cd25dadb42ad049ffb5a91b
Austin L. Howard <austin.lane.howard@gmail.com>
2024-01-18 11:50:19 -0700

    Fixed edge case scenario in module U

---
+++ preliminary_trial.py
@@ -0,0 +1,7 @@
+
+base_length = float(input("Enter the base length of the parallelogram: "))
+height = float(input("Enter the height of the parallelogram: "))
+
+area = base_length * height
+
+print("The area of the parallelogram is", area)
