817d9f210939ca0075cb3331acacce27e3d3059f
Austin L. Howard <austin.lane.howard@gmail.com>
2024-01-19 13:00:51 -0700

    Resolved issue with incorrect data processing

--- preliminary_trial.py
+++
@@ -1,7 +0,0 @@
-
-base_length = float(input("Enter the base length of the parallelogram: "))
-height = float(input("Enter the height of the parallelogram: "))
-
-area = base_length * height
-
-print("The area of the parallelogram is", area)
