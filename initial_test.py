e9f3d0aef97087b0659633a4fa3a9337e81c795f
Austin L. Howard <austin.lane.howard@gmail.com>
2024-01-22 14:49:22 -0700

    Fixed broken links in the application

---
+++ initial_test.py
@@ -0,0 +1,8 @@
+
+base_area = float(input("Enter the base area of the pyramid: ")) # taking input for base area
+height = float(input("Enter the height of the pyramid: ")) # taking input for height
+
+# calculating the area
+area = (base_area * height) / 3
+
+print("The area of the pyramid is:", area)
