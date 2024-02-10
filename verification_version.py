c07159c7f680a9ce757c9f5aee6bc475b3cef648
Austin L. Howard <austin.lane.howard@gmail.com>
2024-02-10 13:34:08 -0700

    Enhanced API error responses for clients

--- verification_version.py
+++ verification_version.py
@@ -1,15 +1,22 @@
 
-def factorial(n):
-    # Base case: factorial of 0 or 1 is 1
-    if n == 0 or n == 1:
-        return 1
-    # Recursive case: multiply n by factorial of (n-1)
-    else:
-        return n * factorial(n - 1)
+import requests
+from bs4 import BeautifulSoup
 
-# Test the function
-num = int(input("Enter a number: "))
-if num < 0:
-    print("Factorial does not exist for negative numbers.")
-else:
-    print("Factorial of", num, "is", factorial(num))
+# URL of the website you want to scrape
+url = 'https://example.com'
+
+# Send a GET request to the website
+response = requests.get(url)
+
+# Parse the HTML content of the website using BeautifulSoup
+soup = BeautifulSoup(response.content, 'html.parser')
+
+# Extract information from the website
+title = soup.title.text # Get the title of the website
+links = soup.find_all('a') # Find all <a> tags (hyperlinks) on the website
+
+# Print the extracted information
+print('Website Title:', title)
+print('Links:')
+for link in links:
+    print(link.get('href'))
