09f1aa890162ee9c1324e6695213c4ed9cee179e
Austin L. Howard <austin.lane.howard@gmail.com>
2024-02-16 12:01:56 -0700

    Updated UI components for a more modern look

---
+++ regression_test.py
@@ -0,0 +1,11 @@
+
+import math
+
+# Take input from the user
+num = float(input("Enter a number: "))
+
+# Use the sqrt() function from the math library to find the square root
+sqrt_num = math.sqrt(num)
+
+# Print the square root
+print("Square root of", num, "is", sqrt_num)
