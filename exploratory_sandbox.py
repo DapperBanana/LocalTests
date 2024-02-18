9d79ea29d3527fee45720ca61c06f8069409e450
Austin L. Howard <austin.lane.howard@gmail.com>
2024-02-18 16:26:06 -0700

    Optimized algorithm B for better performance

---
+++ exploratory_sandbox.py
@@ -0,0 +1,47 @@
+
+def is_valid_sudoku(board):
+    # Check rows
+    for i in range(9):
+        row = set()
+        for j in range(9):
+            if board[i][j] in row:
+                return False
+            row.add(board[i][j])
+    
+    # Check columns
+    for j in range(9):
+        col = set()
+        for i in range(9):
+            if board[i][j] in col:
+                return False
+            col.add(board[i][j])
+    
+    # Check squares
+    for i in range(0, 9, 3):
+        for j in range(0, 9, 3):
+            square = set()
+            for x in range(i, i+3):
+                for y in range(j, j+3):
+                    if board[x][y] in square:
+                        return False
+                    square.add(board[x][y])
+    
+    return True
+
+# Example Sudoku solution
+board = [
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 7, 9]
+]
+
+if is_valid_sudoku(board):
+    print("Valid Sudoku solution")
+else:
+    print("Invalid Sudoku solution")
