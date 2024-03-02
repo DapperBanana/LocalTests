58c49fd91d4588a6b6de0fd455534b3f481d48e7
Austin L. Howard <austin.lane.howard@gmail.com>
2024-03-02 15:23:02 -0700

    Integrated continuous integration workflow for automated testing

---
+++ preliminary_code_run.py
@@ -0,0 +1,9 @@
+
+def sort_list_ascending(lst):
+    lst.sort()
+    return lst
+
+# Test the function
+numbers = [5, 3, 8, 1, 9, 2]
+sorted_numbers = sort_list_ascending(numbers)
+print(sorted_numbers)
