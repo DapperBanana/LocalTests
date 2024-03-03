3cddf2a062d0f7cb01d6dc39ec8a2ec36691e7a0
Austin L. Howard <austin.lane.howard@gmail.com>
2024-03-03 15:39:01 -0700

    Updated configuration settings for environment I

--- mock_check.py
+++
@@ -1,9 +0,0 @@
-
-def calculate_area(length, width):
-    area = length * width
-    return area
-
-length = float(input("Enter the length of the rectangle: "))
-width = float(input("Enter the width of the rectangle: "))
-
-print("The area of the rectangle is:", calculate_area(length, width))
