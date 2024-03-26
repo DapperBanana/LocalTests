16d79c04792eda651b9c40ccf15c62c30c7df978
Austin L. Howard <austin.lane.howard@gmail.com>
2024-03-26 17:30:56 -0600

    Updated project logo and branding

---
+++ baseline_code.py
@@ -0,0 +1,13 @@
+
+def fibonacci(n, memo={}):
+    if n in memo:
+        return memo[n]
+    if n <= 1:
+        memo[n] = n
+        return n
+    else:
+        memo[n] = fibonacci(n-1, memo) + fibonacci(n-2, memo)
+        return memo[n]
+
+n = int(input("Enter the value of n: "))
+print(f"The {n}th term of the Fibonacci sequence is {fibonacci(n)}")
