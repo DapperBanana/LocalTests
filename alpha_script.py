ff47c2f3a6084b247aa4408940e728c25cdb2951
Austin L. Howard <austin.lane.howard@gmail.com>
2024-04-03 11:26:05 -0600

    Updated configuration settings for environment I

---
+++ alpha_script.py
@@ -0,0 +1,21 @@
+
+def is_symmetric(matrix):
+    rows = len(matrix)
+    cols = len(matrix[0])
+    
+    if rows != cols:
+        return False
+    
+    for i in range(rows):
+        for j in range(cols):
+            if matrix[i][j] != matrix[j][i]:
+                return False
+            
+    return True
+
+# Example usage
+matrix = [[1, 2, 3], [2, 4, 5], [3, 5, 6]]
+if is_symmetric(matrix):
+    print("The matrix is symmetric")
+else:
+    print("The matrix is not symmetric")
