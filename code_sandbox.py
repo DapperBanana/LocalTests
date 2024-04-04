eed4dd468e755bee5b1bbf679032ec179031e623
Austin L. Howard <austin.lane.howard@gmail.com>
2024-04-04 17:57:03 -0600

    Implemented API rate limiting for security

--- code_sandbox.py
+++ code_sandbox.py
@@ -1,14 +1,25 @@
 
-def remove_duplicates(lst):
-    seen = set()
-    result = []
-    for item in lst:
-        if item not in seen:
-            seen.add(item)
-            result.append(item)
-    return result
+def edit_distance(str1, str2):
+    m = len(str1)
+    n = len(str2)
+    
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+    
+    for i in range(m + 1):
+        dp[i][0] = i
+    
+    for j in range(n + 1):
+        dp[0][j] = j
+        
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if str1[i-1] == str2[j-1]:
+                dp[i][j] = dp[i-1][j-1]
+            else:
+                dp[i][j] = 1 + min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])
+    
+    return dp[m][n]
 
-# Test the function
-lst = [1, 2, 3, 4, 3, 2, 1, 5]
-result = remove_duplicates(lst)
-print(result)
+str1 = "kitten"
+str2 = "sitting"
+print(f"The edit distance between '{str1}' and '{str2}' is: {edit_distance(str1, str2)}")
