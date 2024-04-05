f59240da7270e81f4b71e9b5a58c7da386cf7ab6
Austin L. Howard <austin.lane.howard@gmail.com>
2024-04-05 14:24:01 -0600

    Optimized resource utilization in feature AA

---
+++ practice_version.py
@@ -0,0 +1,11 @@
+
+def trapezoid_area(base1, base2, height):
+    area = 0.5 * (base1 + base2) * height
+    return area
+
+base1 = float(input("Enter the length of base 1: "))
+base2 = float(input("Enter the length of base 2: "))
+height = float(input("Enter the height: "))
+
+area = trapezoid_area(base1, base2, height)
+print("The area of the trapezoid is: ", area)
