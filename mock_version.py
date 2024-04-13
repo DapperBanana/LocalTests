1d1bd18c765bc143dd815a894893142eb946b1d9
Austin L. Howard <austin.lane.howard@gmail.com>
2024-04-13 13:03:22 -0600

    Optimized network requests for efficiency

---
+++ mock_version.py
@@ -0,0 +1,12 @@
+
+def fibonacci(n, memo={}):
+    if n in memo:
+        return memo[n]
+    if n <= 1:
+        return n
+    memo[n] = fibonacci(n-1, memo) + fibonacci(n-2, memo)
+    return memo[n]
+
+n = int(input("Enter the value of n: "))
+result = fibonacci(n)
+print(f"The {n}th term of the Fibonacci sequence is: {result}")
