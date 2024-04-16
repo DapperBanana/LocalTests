9a3793c5008e9430fcb5d86b7d1ba5ee156720cf
Austin L. Howard <austin.lane.howard@gmail.com>
2024-04-16 16:37:27 -0600

    Fixed memory leaks in feature FF

---
+++ trial_run.py
@@ -0,0 +1,9 @@
+
+def sort_list(lst):
+    lst.sort()
+    return lst
+
+# Test the function with an example list
+example_list = [5, 2, 8, 1, 3]
+sorted_list = sort_list(example_list)
+print(sorted_list)
