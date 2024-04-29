ee1c3e39b132991dd512d9c7883b401aedb9bea7
Austin L. Howard <austin.lane.howard@gmail.com>
2024-04-29 02:51:57 -0600

    Reformatted code for consistency

---
+++ mock_test.py
@@ -0,0 +1,14 @@
+
+def is_palindrome(num):
+    num_str = str(num)
+    if num_str == num_str[::-1]:
+        return True
+    else:
+        return False
+
+num = int(input("Enter a number: "))
+
+if is_palindrome(num):
+    print(f"{num} is a palindrome.")
+else:
+    print(f"{num} is not a palindrome.")
