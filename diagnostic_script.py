480e437359a24359894517bce273c49d28826131
Austin L. Howard <austin.lane.howard@gmail.com>
2024-04-30 15:55:07 -0600

    Implemented asynchronous processing in feature X

---
+++ diagnostic_script.py
@@ -0,0 +1,11 @@
+
+def reverse_string(string):
+    return string[::-1]
+
+# Input string from user
+input_string = input("Enter a string: ")
+
+# Call the function to reverse the string
+reversed_string = reverse_string(input_string)
+
+print("Reversed String: ", reversed_string)
