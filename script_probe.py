1d9394c7f7ba9760734b9189ce51607cca6ae002
Austin L. Howard <austin.lane.howard@gmail.com>
2024-05-01 15:11:33 -0600

    Refactored code to follow design patterns in module BB

--- script_probe.py
+++ script_probe.py
@@ -1,30 +1,14 @@
 
-import numpy as np
+import xml.etree.ElementTree as ET
 
-class LinearRegression:
-    def fit(self, X, y):
-        ones = np.ones((X.shape[0], 1))
-        X = np.concatenate((ones, X), axis=1)
-        
-        self.beta = np.linalg.inv(X.T @ X) @ X.T @ y
-    
-    def predict(self, X):
-        ones = np.ones((X.shape[0], 1))
-        X = np.concatenate((ones, X), axis=1)
-        
-        y_pred = X @ self.beta
-        return y_pred
+# Load and parse the XML file
+tree = ET.parse('data.xml')
+root = tree.getroot()
 
-# Sample data
-X = np.array([[1], [2], [3], [4]])
-y = np.array([2, 4, 6, 8])
-
-# Fit the model
-model = LinearRegression()
-model.fit(X, y)
-
-# Make predictions
-X_test = np.array([[5], [6]])
-y_pred = model.predict(X_test)
-
-print("Predictions:", y_pred)
+# Extract specific information from the XML file
+for child in root:
+    if child.tag == 'person':
+        name = child.find('name').text
+        age = child.find('age').text
+        email = child.find('email').text
+        print(f"Name: {name}, Age: {age}, Email: {email}")
