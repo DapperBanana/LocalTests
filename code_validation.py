3513731b14d63babfd41ffb5c28e41830308a274
Austin L. Howard <austin.lane.howard@gmail.com>
2024-05-04 14:06:59 -0600

    Updated project dependencies for security patches

---
+++ code_validation.py
@@ -0,0 +1,11 @@
+
+def calculate_pyramid_area(base_area, height):
+    # Calculate the area of the pyramid using the formula: (1/3) * base area * height
+    pyramid_area = (1/3) * base_area * height
+    return pyramid_area
+
+base_area = float(input("Enter the base area of the pyramid: "))
+height = float(input("Enter the height of the pyramid: "))
+
+area = calculate_pyramid_area(base_area, height)
+print("The area of the pyramid is: ", area)
