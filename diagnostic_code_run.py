ca6b7a9bdf24ae4d2eca920f56956a1b44152fac
Austin L. Howard <austin.lane.howard@gmail.com>
2024-05-09 13:50:21 -0600

    Removed unnecessary debug statements

--- diagnostic_code_run.py
+++
@@ -1,7 +0,0 @@
-letion(id='chatcmpl-9MjfPZXA7qS1GyQK60ysxUTLqLoty', choices=[Choice(finish_reason='stop', index=0, logprobs=None, message=ChatCompletionMessage(content='def convert_to_title_case(input_string):
-    return input_string.title()
-
-# Test the function
-input_string = "hello world"
-output_string = convert_to_title_case(input_string)
-print(output_string)', role='assistant', function_call=None, tool_calls=None))], created=1715205787, model='gpt-3.5-turbo-0125', object='chat.completion', system_fingerprint=None, usage=CompletionUsage(completion_tokens=40, prompt_tokens=19, total_tokens=59)
--- smoke_test.py
+++
@@ -1,9 +0,0 @@
-letion(id='chatcmpl-9Mjgop8aOyxmfpNfHf7lmN9D0ak37', choices=[Choice(finish_reason='stop', index=0, logprobs=None, message=ChatCompletionMessage(content='import math
-
-def calculate_tetrahedron_area(side_length):
-    area = math.sqrt(3) * side_length**2
-    return area
-
-side_length = float(input("Enter the side length of the tetrahedron: "))
-area = calculate_tetrahedron_area(side_length)
-print("The area of the regular tetrahedron is: ", area)', role='assistant', function_call=None, tool_calls=None))], created=1715205874, model='gpt-3.5-turbo-0125', object='chat.completion', system_fingerprint=None, usage=CompletionUsage(completion_tokens=78, prompt_tokens=23, total_tokens=101)
