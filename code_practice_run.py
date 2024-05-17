3f8ba6f43f63d2463887a7e9de59e033522088be
Austin L. Howard <austin.lane.howard@gmail.com>
2024-05-17 13:54:12 -0600

    Added missing error handling in module L

---
+++ code_practice_run.py
@@ -0,0 +1,10 @@
+
+def sum_of_elements(lst):
+    total = 0
+    for num in lst:
+        total += num
+    return total
+
+# Test the function
+my_list = [1, 2, 3, 4, 5]
+print("Sum of all elements in the list is:", sum_of_elements(my_list))
