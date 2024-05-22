cfd91bd3c533de6e2eee82b61e1aa9ec40607d14
Austin L. Howard <austin.lane.howard@gmail.com>
2024-05-21 18:18:44 -0600

    Refactored code to follow design patterns in module BB

--- mock_test.py
+++
@@ -1,8 +0,0 @@
-letion(id='chatcmpl-9R1sMza5V6twNu6pwMI9dETfhb9Ed', choices=[Choice(finish_reason='stop', index=0, logprobs=None, message=ChatCompletionMessage(content='import math
-
-def area_of_octagon(side_length):
-    area = 2 * (1 + math.sqrt(2)) * side_length ** 2
-    return area
-
-side_length = float(input("Enter the side length of the octagon: "))
-print("The area of the regular octagon is: ", area_of_octagon(side_length))', role='assistant', function_call=None, tool_calls=None))], created=1716229094, model='gpt-3.5-turbo-0125', object='chat.completion', system_fingerprint=None, usage=CompletionUsage(completion_tokens=71, prompt_tokens=21, total_tokens=92)
--- prototype_test.py
+++
@@ -1,6 +0,0 @@
-letion(id='chatcmpl-9R1rFbXCJOozVUpZ2ZnI6vTJRU3kP', choices=[Choice(finish_reason='stop', index=0, logprobs=None, message=ChatCompletionMessage(content='num = int(input("Enter a number: "))
-
-if num % 2 == 0:
-    print(f"{num} is even.")
-else:
-    print(f"{num} is odd.")', role='assistant', function_call=None, tool_calls=None))], created=1716229025, model='gpt-3.5-turbo-0125', object='chat.completion', system_fingerprint=None, usage=CompletionUsage(completion_tokens=40, prompt_tokens=21, total_tokens=61)
