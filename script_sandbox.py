4dee05bdc03a89f3a985ccdef0f54898aad81aa1
Austin L. Howard <austin.lane.howard@gmail.com>
2024-05-27 19:07:41 -0600

    Updated project dependencies for security patches

---
+++ script_sandbox.py
@@ -0,0 +1,20 @@
+
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def count_primes_in_range(start, end):
+    count = 0
+    for num in range(start, end + 1):
+        if is_prime(num):
+            count += 1
+    return count
+
+start_range = int(input("Enter the start of the range: "))
+end_range = int(input("Enter the end of the range: "))
+
+print(f"Number of prime numbers in the range {start_range} to {end_range} is: {count_primes_in_range(start_range, end_range)}")
