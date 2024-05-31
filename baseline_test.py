2605e4fd2397049472398987d848dbd0b75b437e
Austin L. Howard <austin.lane.howard@gmail.com>
2024-05-31 12:00:50 -0600

    Fixed memory leaks in feature FF

---
+++ baseline_test.py
@@ -0,0 +1,16 @@
+
+def prime_factors(n):
+    factors = []
+    i = 2
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
+
+num = int(input("Enter a number to find its prime factors: "))
+print("Prime factors of", num, "are:", prime_factors(num))
