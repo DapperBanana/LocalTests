88cdec0a401a5708e12e51929de03eb73e113a0e
Austin L. Howard <austin.lane.howard@gmail.com>
2024-06-01 15:26:05 -0600

    Implemented data caching for faster retrieval

---
+++ preliminary_code_run.py
@@ -0,0 +1,14 @@
+
+def decimal_to_binary(decimal_num):
+    if decimal_num == 0:
+        return '0'
+    binary_num = ''
+    while decimal_num > 0:
+        remainder = decimal_num % 2
+        binary_num = str(remainder) + binary_num
+        decimal_num //= 2
+    return binary_num
+
+decimal_num = int(input("Enter a decimal number: "))
+binary_num = decimal_to_binary(decimal_num)
+print(f"The binary representation of {decimal_num} is {binary_num}")
