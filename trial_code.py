85975d132b38b3cc522a25cc898331dce609cf5e
Austin L. Howard <austin.lane.howard@gmail.com>
2024-06-03 17:38:35 -0600

    Resolved cross-browser compatibility issues

--- trial_code.py
+++ trial_code.py
@@ -1,32 +1,29 @@
 
-def is_prime(n):
-    if n <= 1:
-        return False
-    if n <= 3:
-        return True
-    if n % 2 == 0 or n % 3 == 0:
-        return False
-    i = 5
-    while i * i <= n:
-        if n % i == 0 or n % (i + 2) == 0:
-            return False
-        i += 6
-    return True
-
-def is_lucas_carmichael(n):
-    if n == 2:
-        return False
-    for i in range(1, n):
-        if pow(2, i, n) != 1:
-            return False
-    if is_prime(n) and n % 4 == 3:
-        return True
+def find_lcm(x, y):
+    if x > y:
+        greater = x
     else:
-        return False
+        greater = y
 
-num = int(input("Enter a number: "))
+    while True:
+        if greater % x == 0 and greater % y == 0:
+            lcm = greater
+            break
+        greater += 1
 
-if is_lucas_carmichael(num):
-    print(f"{num} is a Lucas-Carmichael number.")
-else:
-    print(f"{num} is not a Lucas-Carmichael number.")
+    return lcm
+
+def calculate_lcm(numbers):
+    lcm = numbers[0]
+    for i in range(1, len(numbers)):
+        lcm = find_lcm(lcm, numbers[i])
+
+    return lcm
+
+# Input list of numbers
+numbers = [4, 6, 8, 10, 12]
+
+# Calculate LCM
+result = calculate_lcm(numbers)
+
+print(f"The LCM of the numbers {numbers} is: {result}")
