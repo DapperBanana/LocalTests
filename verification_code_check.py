3ba5a5a32b2f4a072c7788a359c4bdf15d30f642
Austin L. Howard <austin.lane.howard@gmail.com>
2024-06-13 14:09:08 -0600

    Implemented user permission checks in module JJ

--- verification_code_check.py
+++ verification_code_check.py
@@ -1,18 +1,21 @@
 
-def hamming_distance(str1, str2):
-    if len(str1) != len(str2):
-        raise ValueError("The two strings must have the same length.")
+def take_quiz():
+    questions = [
+        {"question": "What is the capital of France?", "answer": "Paris"},
+        {"question": "What is 2+2?", "answer": "4"},
+        {"question": "Who wrote Romeo and Juliet?", "answer": "Shakespeare"}
+    ]
     
-    distance = 0
-    for char1, char2 in zip(str1, str2):
-        if char1 != char2:
-            distance += 1
+    score = 0
     
-    return distance
-
-# Input strings
-str1 = "karolin"
-str2 = "kathrin"
-
-# Calculate and print Hamming distance
-print("Hamming distance between '{}' and '{}': {}".format(str1, str2, hamming_distance(str1, str2)))
+    for question in questions:
+        user_answer = input(question["question"] + "\n")
+        if user_answer.lower() == question["answer"].lower():
+            print("Correct!")
+            score += 1
+        else:
+            print("Incorrect. The correct answer is: " + question["answer"])
+    
+    print("You got " + str(score) + " out of " + str(len(questions)) + " questions correct.")
+    
+take_quiz()
