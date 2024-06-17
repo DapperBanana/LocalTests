3e09269c3d90feb39e148d14abfeb829f816453b
Austin L. Howard <austin.lane.howard@gmail.com>
2024-06-17 12:49:24 -0600

    Added missing input validation for module DD

--- system_test.py
+++
@@ -1,8 +0,0 @@
-
-def decimal_to_binary(decimal_num):
-    binary_num = bin(decimal_num).replace("0b", "")
-    return binary_num
-
-decimal_num = int(input("Enter a decimal number: "))
-binary_num = decimal_to_binary(decimal_num)
-print(f"The binary equivalent of {decimal_num} is {binary_num}")
