04148ab4d2a966b44ccaa55b04767127167342d4
Austin L. Howard <austin.lane.howard@gmail.com>
2024-06-30 13:41:21 -0600

    Updated project dependencies for new features

---
+++ script_run.py
@@ -0,0 +1,26 @@
+
+def is_symmetric(matrix):
+    num_rows = len(matrix)
+    num_cols = len(matrix[0])
+    
+    if num_rows != num_cols:
+        return False
+        
+    for i in range(num_rows):
+        for j in range(num_cols):
+            if matrix[i][j] != matrix[j][i]:
+                return False
+                
+    return True
+
+# Test the function
+matrix = [
+    [1, 2, 3],
+    [2, 4, 5],
+    [3, 5, 6]
+]
+
+if is_symmetric(matrix):
+    print("The matrix is symmetric")
+else:
+    print("The matrix is not symmetric")
