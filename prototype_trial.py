317183b080b8416eb4b1eabf91b75a9738b23be8
Austin L. Howard <austin.lane.howard@gmail.com>
2024-06-30 13:35:10 -0600

    Removed unused assets from the project

---
+++ prototype_trial.py
@@ -0,0 +1,11 @@
+
+def is_harshad_number(num):
+    sum_of_digits = sum(int(digit) for digit in str(num))
+    return num % sum_of_digits == 0
+
+num = int(input("Enter a number: "))
+
+if is_harshad_number(num):
+    print(num, "is a Harshad (Niven) number.")
+else:
+    print(num, "is not a Harshad (Niven) number.")
