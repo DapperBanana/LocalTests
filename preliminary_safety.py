1cc4c0193203d5c9455c2fa20d11fe91348d5138
Austin L. Howard <austin.lane.howard@gmail.com>
2024-07-02 22:17:57 -0600

    Implemented pagination for large datasets

---
+++ preliminary_safety.py
@@ -0,0 +1,15 @@
+
+a = 5
+b = 10
+
+print("Original values:")
+print("a =", a)
+print("b =", b)
+
+a = a + b
+b = a - b
+a = a - b
+
+print("\nValues after swapping:")
+print("a =", a)
+print("b =", b)
