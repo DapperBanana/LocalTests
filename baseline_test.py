1ba3b362bf749ddd519960ec5194cf1e5149254a
Austin L. Howard <austin.lane.howard@gmail.com>
2024-07-03 17:44:09 -0600

    Fixed broken links in the application

---
+++ baseline_test.py
@@ -0,0 +1,18 @@
+
+import matplotlib.pyplot as plt
+
+def generate_bar_chart(data):
+    keys = list(data.keys())
+    values = list(data.values())
+    
+    plt.bar(keys, values)
+    plt.xlabel('Categories')
+    plt.ylabel('Values')
+    plt.title('Simple Bar Chart')
+    
+    plt.show()
+
+# Sample data
+data = {'A': 10, 'B': 20, 'C': 15, 'D': 25}
+
+generate_bar_chart(data)
