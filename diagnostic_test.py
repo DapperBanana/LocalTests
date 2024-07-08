296fe1885cfd43ce84fe30c26798846f49dd4109
Austin L. Howard <austin.lane.howard@gmail.com>
2024-07-08 17:59:28 -0600

    Resolved issue with incorrect data processing

---
+++ diagnostic_test.py
@@ -0,0 +1,14 @@
+
+import math
+
+def find_square_root(num):
+    return math.sqrt(num)
+
+# Input
+num = float(input("Enter a number: "))
+
+# Find square root
+square_root = find_square_root(num)
+
+# Output
+print(f"The square root of {num} is {square_root}")
