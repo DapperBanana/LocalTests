9747e51a7ee664266348f6812201536bb670b36f
Austin L. Howard <austin.lane.howard@gmail.com>
2024-07-12 11:11:49 -0600

    Integrated automated code formatting

--- safety_version.py
+++
@@ -1,9 +0,0 @@
-
-def decimal_to_binary(decimal_num):
-    binary_num = bin(decimal_num)[2:]
-    return binary_num
-
-decimal_num = int(input("Enter a decimal number: "))
-binary_num = decimal_to_binary(decimal_num)
-
-print(f"The binary representation of {decimal_num} is: {binary_num}")
