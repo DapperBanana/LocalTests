05e9192c236fc19d5dc1cc9976e71bb8b107ed5a
Austin L. Howard <austin.lane.howard@gmail.com>
2024-07-23 13:02:53 -0600

    Removed unused assets from the project

---
+++ trial_version.py
@@ -0,0 +1,10 @@
+
+def sort_list(nums):
+    nums.sort()
+    return nums
+
+# Test the function
+nums = [5, 2, 8, 1, 3]
+sorted_nums = sort_list(nums)
+print("Original List:", nums)
+print("Sorted List:", sorted_nums)
