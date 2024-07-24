9d15f158e91f407cd3202e614d89058adbe7bae3
Austin L. Howard <austin.lane.howard@gmail.com>
2024-07-24 13:02:41 -0600

    Refactored CSS for a more responsive design

---
+++ script_trial.py
@@ -0,0 +1,13 @@
+
+import math
+
+def find_square_root(num):
+    return math.sqrt(num)
+
+# Taking input from user
+num = float(input("Enter a number: "))
+
+# Finding square root of the number
+result = find_square_root(num)
+
+print(f"The square root of {num} is {result}")
