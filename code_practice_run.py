08e83f16b332c260e9c49d7a4bec4c322c1f0846
Austin L. Howard <austin.lane.howard@gmail.com>
2024-07-26 18:54:58 -0600

    Optimized database queries in module V

---
+++ code_practice_run.py
@@ -0,0 +1,26 @@
+
+def is_symmetric(matrix):
+    row_len = len(matrix)
+    col_len = len(matrix[0])
+    
+    if row_len != col_len:
+        return False
+    
+    for i in range(row_len):
+        for j in range(i+1, col_len):
+            if matrix[i][j] != matrix[j][i]:
+                return False
+    
+    return True
+
+# Example test case
+matrix = [
+    [1, 2, 3],
+    [2, 4, 5],
+    [3, 5, 6]
+]
+
+if is_symmetric(matrix):
+    print("The matrix is symmetric")
+else:
+    print("The matrix is not symmetric")
