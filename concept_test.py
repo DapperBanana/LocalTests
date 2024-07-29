7f64a517cba896b0f41203acc59309245310fc28
Austin L. Howard <austin.lane.howard@gmail.com>
2024-07-28 18:30:33 -0600

    Added logging for enhanced debugging in module W

---
+++ concept_test.py
@@ -0,0 +1,14 @@
+
+# Function to sort a list of integers in ascending order
+def sort_list_ascending(input_list):
+    sorted_list = sorted(input_list)
+    return sorted_list
+
+# Input list of integers
+input_list = [12, 45, 78, 23, 56, 90, 34, 67]
+
+# Sort the input list in ascending order
+sorted_list = sort_list_ascending(input_list)
+
+# Display the sorted list
+print("Sorted list in ascending order:", sorted_list)
