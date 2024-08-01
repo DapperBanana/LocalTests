63bb0791c70ce7eb6d224c73e64fc69414417565
Austin L. Howard <austin.lane.howard@gmail.com>
2024-08-01 12:57:31 -0600

    Reverted changes in file Q for compatibility

--- code_sandbox.py
+++ code_sandbox.py
@@ -1,35 +1,13 @@
 
-def is_magic_square(matrix):
-    n = len(matrix)
-    total_sum = sum(matrix[0])
-    
-    # Check sum of rows
-    for row in matrix:
-        if sum(row) != total_sum:
-            return False
-    
-    # Check sum of columns
-    for i in range(n):
-        col_sum = sum(row[i] for row in matrix)
-        if col_sum != total_sum:
-            return False
-    
-    # Check sum of diagonals
-    diag_sum1 = sum(matrix[i][i] for i in range(n))
-    diag_sum2 = sum(matrix[i][n-i-1] for i in range(n))
-    if diag_sum1 != total_sum or diag_sum2 != total_sum:
-        return False
-    
-    return True
+def gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a % b)
 
-# Example matrix
-matrix = [
-    [2, 7, 6],
-    [9, 5, 1],
-    [4, 3, 8]
-]
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 
-if is_magic_square(matrix):
-    print("The given matrix is a magic square.")
-else:
-    print("The given matrix is not a magic square.")
+result = gcd(num1, num2)
+
+print(f"The greatest common divisor of {num1} and {num2} is {result}")
