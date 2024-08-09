790e7fde7b002ec394072d7331830ccac68d0e9d
Austin L. Howard <austin.lane.howard@gmail.com>
2024-08-08 18:00:24 -0600

    Refactored code for better modularity in module Y

--- script_sandbox.py
+++
@@ -1,9 +0,0 @@
-
-import math
-
-def area_of_hexagon(side_length):
-    return ((3 * math.sqrt(3)) / 2) * (side_length ** 2)
-
-side_length = float(input("Enter the side length of the regular hexagon: "))
-area = area_of_hexagon(side_length)
-print(f"The area of the regular hexagon is: {area}")
