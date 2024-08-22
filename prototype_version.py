f4f94a62ab7608f5d433c5d29432af888c91dbb5
Austin L. Howard <austin.lane.howard@gmail.com>
2024-08-22 15:25:12 -0600

    Removed deprecated function H

--- prototype_version.py
+++ prototype_version.py
@@ -1,40 +1,22 @@
 
-import zlib
+import numpy as np
 
-def compress_file(input_file, output_file):
-    try:
-        with open(input_file, 'rb') as f:
-            data = f.read()
-            
-            compressed_data = zlib.compress(data)
-            
-            with open(output_file, 'wb') as out:
-                out.write(compressed_data)
-                
-        print("File compressed successfully!")
-        
-    except FileNotFoundError:
-        print("File not found!")
+def is_orthogonal(matrix):
+    rows, cols = matrix.shape
+    
+    if rows != cols:
+        return False
+    
+    orthogonal = np.allclose(np.dot(matrix, matrix.T), np.eye(rows))
+    
+    return orthogonal
 
-def decompress_file(input_file, output_file):
-    try:
-        with open(input_file, 'rb') as f:
-            compressed_data = f.read()
-            
-            decompressed_data = zlib.decompress(compressed_data)
-            
-            with open(output_file, 'wb') as out:
-                out.write(decompressed_data)
-                
-        print("File decompressed successfully!")
-        
-    except FileNotFoundError:
-        print("File not found!")
+# Example matrix to test
+A = np.array([[1, 0, 0],
+              [0, 1, 0],
+              [0, 0, -1]])
 
-# Example usage
-input_file = 'example.txt'
-compressed_file = 'example.txt.gz'
-decompressed_file = 'example_decompressed.txt'
-
-compress_file(input_file, compressed_file)
-decompress_file(compressed_file, decompressed_file)
+if is_orthogonal(A):
+    print("The matrix is orthogonal.")
+else:
+    print("The matrix is not orthogonal.")
