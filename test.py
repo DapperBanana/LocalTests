89fbe72d2d9e683055f7c314647e59993343d544
Austin L. Howard <austin.lane.howard@gmail.com>
2024-08-21 18:59:16 -0600

    Implemented caching strategy for performance gains

--- test.py
+++
@@ -1,9 +0,0 @@
-
-def square_elements(lst):
-    squared_lst = [num ** 2 for num in lst]
-    return squared_lst
-
-# Test the function
-input_list = [1, 2, 3, 4, 5]
-output_list = square_elements(input_list)
-print(output_list)
