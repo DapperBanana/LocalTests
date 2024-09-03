6e0e13a9dd350975936f448dc4a9a2a080c8a2b6
Austin L. Howard <austin.lane.howard@gmail.com>
2024-09-03 16:02:38 -0600

    Refactored code to improve code readability

--- safety_run.py
+++ safety_run.py
@@ -1,24 +1,10 @@
 
-def encrypt(text, key):
-    encrypted_text = ""
-    for char in text:
-        if char.isalpha():
-            offset = 65 if char.isupper() else 97
-            encrypted_char = chr((ord(char) - offset + key) % 26 + offset)
-            encrypted_text += encrypted_char
-        else:
-            encrypted_text += char
-    return encrypted_text
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
 
-def decrypt(text, key):
-    return encrypt(text, -key)
-
-if __name__ == "__main__":
-    text = "Hello, World!"
-    key = 3
-
-    encrypted_text = encrypt(text, key)
-    print("Encrypted text:", encrypted_text)
-
-    decrypted_text = decrypt(encrypted_text, key)
-    print("Decrypted text:", decrypted_text)
+num = int(input("Enter a number: "))
+result = factorial(num)
+print(f"The factorial of {num} is {result}")
