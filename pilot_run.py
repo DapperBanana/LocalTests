21346c526f2ea4c30dfc4dadd30f395a89d8af3d
Austin L. Howard <austin.lane.howard@gmail.com>
2024-09-05 18:13:27 -0600

    Implemented pagination for large datasets

---
+++ pilot_run.py
@@ -0,0 +1,20 @@
+
+def is_prime(num):
+    if num == 1:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def sum_of_primes(start, end):
+    total = 0
+    for num in range(start, end + 1):
+        if is_prime(num):
+            total += num
+    return total
+
+start_range = int(input("Enter the start of the range: "))
+end_range = int(input("Enter the end of the range: "))
+
+print("The sum of all prime numbers in the range", start_range, "to", end_range, "is:", sum_of_primes(start_range, end_range))
