e0e87542c32060d52f4365ef08bea89af5b240a2
Austin L. Howard <austin.lane.howard@gmail.com>
2024-09-13 14:56:22 -0600

    Optimized database schema for improved efficiency

---
+++ code_experiment.py
@@ -0,0 +1,16 @@
+
+def is_narcissistic_number(num):
+    num_str = str(num)
+    power = len(num_str)
+    total = 0
+
+    for digit in num_str:
+        total += int(digit) ** power
+
+    return total == num
+
+num = int(input("Enter a number: "))
+if is_narcissistic_number(num):
+    print(f"{num} is a narcissistic number")
+else:
+    print(f"{num} is not a narcissistic number")
