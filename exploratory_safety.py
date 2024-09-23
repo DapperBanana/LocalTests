d5d8925ad9cbaec3a898343c0dc35e840278c9f9
Austin L. Howard <austin.lane.howard@gmail.com>
2024-09-23 13:22:57 -0600

    Optimized algorithm B for better performance

---
+++ exploratory_safety.py
@@ -0,0 +1,9 @@
+
+def square_elements(lst):
+    squared_list = [x**2 for x in lst]
+    return squared_list
+
+# Example
+my_list = [1, 2, 3, 4, 5]
+squared_values = square_elements(my_list)
+print(squared_values)
