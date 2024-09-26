29fe0c9aceb29923c15361f7235665401ced4526
Austin L. Howard <austin.lane.howard@gmail.com>
2024-09-26 14:23:03 -0600

    Optimized database queries in module V

---
+++ baseline_experiment.py
@@ -0,0 +1,9 @@
+
+def intersection(lst1, lst2):
+    return list(set(lst1) & set(lst2)
+
+list1 = [1, 2, 3, 4, 5]
+list2 = [3, 4, 5, 6, 7]
+
+intersection_list = intersection(list1, list2)
+print(intersection_list)
