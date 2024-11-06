5eb913789900d84550dd43b85878c32a3549ce35
Austin L. Howard <austin.lane.howard@gmail.com>
2024-11-06 12:18:31 -0700

    Integrated feedback from code review

--- test_suite.py
+++
@@ -1,9 +0,0 @@
-
-def intersection(lst1, lst2):
-    return list(set(lst1) & set(lst2))
-
-list1 = [1, 2, 3, 4, 5]
-list2 = [4, 5, 6, 7, 8]
-
-intersect = intersection(list1, list2)
-print("Intersection of list1 and list2:", intersect)
