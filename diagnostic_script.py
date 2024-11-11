d284e0b01804a4ed1a7269cb7a650bfcb81c3c43
Austin L. Howard <austin.lane.howard@gmail.com>
2024-11-11 12:46:42 -0700

    Updated project dependencies for new features

--- diagnostic_script.py
+++
@@ -1,9 +0,0 @@
-
-def calculate_average(lst):
-    total = sum(lst)
-    average = total / len(lst)
-    return average
-
-# Test the function with a sample list
-numbers = [5, 10, 15, 20, 25]
-print("The average of the numbers in the list is:", calculate_average(numbers))
---
+++ exploratory_trial.py
@@ -0,0 +1,17 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Set the number of steps in the random walk
+num_steps = 1000
+
+# Generate random walk data
+x = np.cumsum(np.random.randn(num_steps))
+y = np.cumsum(np.random.randn(num_steps))
+
+# Plot the random walk
+plt.plot(x, y)
+plt.title('Random Walk Plot')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.show()
