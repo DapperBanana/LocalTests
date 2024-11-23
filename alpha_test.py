a7df3671c4014bb8728829ea46dc02233935a488
Austin L. Howard <austin.lane.howard@gmail.com>
2024-11-23 14:03:01 -0700

    Added new feature Z

---
+++ alpha_test.py
@@ -0,0 +1,23 @@
+
+import qrcode
+
+# Get input from the user
+data = input("Enter the data for the QR code: ")
+
+# Generate the QR code
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data(data)
+qr.make(fit=True)
+
+# Create an image from the QR code
+img = qr.make_image(fill_color="black", back_color="white")
+
+# Save the QR code image
+img.save("qr_code.png")
+
+print("QR code generated and saved as qr_code.png")
