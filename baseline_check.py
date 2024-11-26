6778d5787c4dc0061e491cb69153a8647a7aeddb
Austin L. Howard <austin.lane.howard@gmail.com>
2024-11-26 14:43:57 -0700

    Removed redundant code in module EE

---
+++ baseline_check.py
@@ -0,0 +1,9 @@
+
+def calculate_average(numbers):
+    total = sum(numbers)
+    return total / len(numbers)
+
+# Test the program
+numbers = [1, 2, 3, 4, 5]
+average = calculate_average(numbers)
+print("The average of the numbers is:", average)
