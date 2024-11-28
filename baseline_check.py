4c67ab17a0b7a6a31b8494dd61459787b2c79610
Austin L. Howard <austin.lane.howard@gmail.com>
2024-11-27 18:29:25 -0700

    Fixed bug in module Y

--- baseline_check.py
+++
@@ -1,9 +0,0 @@
-
-def calculate_average(numbers):
-    total = sum(numbers)
-    return total / len(numbers)
-
-# Test the program
-numbers = [1, 2, 3, 4, 5]
-average = calculate_average(numbers)
-print("The average of the numbers is:", average)
