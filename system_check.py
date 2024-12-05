fb35e320cab0ca47678259aaf5593d28e231591b
Austin L. Howard <austin.lane.howard@gmail.com>
2024-12-05 11:49:18 -0700

    Refined data validation for input M

---
+++ system_check.py
@@ -0,0 +1,16 @@
+
+import random
+import string
+
+def generate_password(length):
+    password = ''
+    characters = string.ascii_letters + string.digits + string.punctuation
+    
+    for _ in range(length):
+        password += random.choice(characters)
+    
+    return password
+
+length = int(input("Enter the length of the password: "))
+password = generate_password(length)
+print(f"Random Password: {password}")
