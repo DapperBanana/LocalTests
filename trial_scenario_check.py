3b3d3513a0074916526eeaa9ef4b7b95cce9e7d9
Austin L. Howard <austin.lane.howard@gmail.com>
2024-12-08 15:40:33 -0700

    Merged feature branch N into main

--- trial_scenario_check.py
+++ trial_scenario_check.py
@@ -1,22 +1,13 @@
 
-def get_matrix_minor(matrix, i, j):
-    return [row[:j] + row[j+1:] for row in (matrix[:i] + matrix[i+1:])]
+def gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a % b)
 
-def get_matrix_determinant(matrix):
-    if len(matrix) == 2:
-        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
-    
-    determinant = 0
-    
-    for c in range(len(matrix)):
-        determinant += ((-1) ** c) * matrix[0][c] * get_matrix_determinant(get_matrix_minor(matrix, 0, c))
-    
-    return determinant
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 
-# Input a square matrix
-matrix = [[1, 2, 3],
-          [4, 5, 6],
-          [7, 8, 9]]
+result = gcd(num1, num2)
 
-determinant = get_matrix_determinant(matrix)
-print(f"Determinant of the matrix is: {determinant}")
+print(f"The greatest common divisor (GCD) of {num1} and {num2} is: {result}")
