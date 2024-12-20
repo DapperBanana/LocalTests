df0eead320cfae5a32eeaa9d1148c4d771822944
Austin L. Howard <austin.lane.howard@gmail.com>
2024-12-19 19:04:39 -0700

    Resolved cross-browser compatibility issues

---
+++ prototype_run.py
@@ -0,0 +1,14 @@
+
+def is_perfect_digital_invariant(num):
+    num_str = str(num)
+    total = 0
+    for digit in num_str:
+        total += int(digit) ** int(digit)
+    
+    return total == num
+
+num = int(input("Enter a number: "))
+if is_perfect_digital_invariant(num):
+    print(f"{num} is a perfect digital invariant")
+else:
+    print(f"{num} is not a perfect digital invariant")
