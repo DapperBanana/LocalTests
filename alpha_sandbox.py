a492c9be6930be47ea3902f32fda6624dd817766
Austin L. Howard <austin.lane.howard@gmail.com>
2024-12-19 19:05:43 -0700

    Added support for user roles in the application

---
+++ alpha_sandbox.py
@@ -0,0 +1,9 @@
+
+def check_even_odd(num):
+    if num % 2 == 0:
+        print(f"{num} is an even number.")
+    else:
+        print(f"{num} is an odd number.")
+
+num = int(input("Enter a number: "))
+check_even_odd(num)
