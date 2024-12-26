c1dff8af4caf7f652131d541a71bc052d9be3805
Austin L. Howard <austin.lane.howard@gmail.com>
2024-12-25 18:52:22 -0700

    Enhanced error logging for production monitoring

---
+++ mock_check.py
@@ -0,0 +1,15 @@
+
+import math
+
+def find_square_root(num):
+    if num < 0:
+        return "Square root of a negative number is not possible"
+    else:
+        return math.sqrt(num)
+
+# taking user input
+num = float(input("Enter a number: "))
+
+result = find_square_root(num)
+
+print(f"The square root of {num} is {result}")
