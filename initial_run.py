0c6ecd5954a980a9783e7ef150770baf0e8c6133
Austin L. Howard <austin.lane.howard@gmail.com>
2024-12-27 15:37:53 -0700

    Implemented error handling for edge case E

---
+++ initial_run.py
@@ -0,0 +1,10 @@
+
+import math
+
+def square_root(num):
+    return math.sqrt(num)
+
+num = float(input("Enter a number: "))
+result = square_root(num)
+
+print(f"The square root of {num} is {result}")
