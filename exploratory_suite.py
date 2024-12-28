81ece6f280647d2a02a17ec7fb6770a711ba2345
Austin L. Howard <austin.lane.howard@gmail.com>
2024-12-28 12:13:10 -0700

    Optimized network requests for efficiency

---
+++ exploratory_suite.py
@@ -0,0 +1,7 @@
+
+def convert_to_title_case(text):
+    return text.title()
+
+input_string = input("Enter a string: ")
+title_case_string = convert_to_title_case(input_string)
+print("String in title case: ", title_case_string)
