ade3b137cbf361129f974f9dbf27ddbbd02551c9
Austin L. Howard <austin.lane.howard@gmail.com>
2025-01-01 13:58:42 -0700

    Fixed accessibility issues in the UI

---
+++ prototype_check.py
@@ -0,0 +1,10 @@
+
+def find_union(set1, set2):
+    union_set = set1.union(set2)
+    return union_set
+
+# Test the function
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+
+print(find_union(set1, set2))
