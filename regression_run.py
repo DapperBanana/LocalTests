361e1ad3a3ccc94487498412bd769041da2ae937
Austin L. Howard <austin.lane.howard@gmail.com>
2025-01-07 16:08:12 -0700

    Resolved cross-browser compatibility issues

--- regression_run.py
+++
@@ -1,9 +0,0 @@
-
-def reverse_sentence(sentence):
-    words = sentence.split()
-    reversed_sentence = ' '.join(reversed(words))
-    return reversed_sentence
-
-sentence = input("Enter a sentence: ")
-reversed_sentence = reverse_sentence(sentence)
-print("Reversed sentence:", reversed_sentence)
