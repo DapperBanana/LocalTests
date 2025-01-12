59a4e4ae6bc395b61b1b2dd3044d7d0f37538796
Austin L. Howard <austin.lane.howard@gmail.com>
2025-01-11 17:20:01 -0700

    Fixed bug in module Y

---
+++ exploratory_validation.py
@@ -0,0 +1,9 @@
+
+def reverse_words(sentence):
+    words = sentence.split()
+    reversed_sentence = " ".join(reversed(words))
+    return reversed_sentence
+
+user_input = input("Enter a sentence: ")
+reversed_sentence = reverse_words(user_input)
+print("Reversed sentence:", reversed_sentence)
