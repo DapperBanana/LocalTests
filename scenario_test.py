fc3e40304b472a4ca97ea897938071ec091c3447
Austin L. Howard <austin.lane.howard@gmail.com>
2025-01-15 11:25:03 -0700

    Fixed accessibility issues in the UI

---
+++ scenario_test.py
@@ -0,0 +1,19 @@
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def sum_of_primes(limit):
+    prime_sum = 0
+    for num in range(2, limit + 1):
+        if is_prime(num):
+            prime_sum += num
+    return prime_sum
+
+limit = int(input("Enter a limit: "))
+result = sum_of_primes(limit)
+print("Sum of all prime numbers up to", limit, "is:", result)
