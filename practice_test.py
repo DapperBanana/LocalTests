1ab758bdb7fb417f4a8cc925b72c8036baf8686a
Austin L. Howard <austin.lane.howard@gmail.com>
2025-01-16 18:31:56 -0700

    Updated error messages for better user understanding

---
+++ practice_test.py
@@ -0,0 +1,9 @@
+
+def sum_of_list(lst):
+    total = sum(lst)
+    return total
+
+# Example list
+numbers = [10, 20, 30, 40, 50]
+total_sum = sum_of_list(numbers)
+print("The sum of all elements in the list is:", total_sum)
