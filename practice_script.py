70e6e93d1af391e068216af8280b5dcadaf6999f
Austin L. Howard <austin.lane.howard@gmail.com>
2025-01-20 16:07:12 -0700

    Removed unnecessary debug statements

--- practice_script.py
+++ practice_script.py
@@ -1,19 +1,23 @@
 
 def gcd(a, b):
-    while b:
+    while b != 0:
         a, b = b, a % b
     return a
 
+def is_coprime(x, y):
+    return gcd(x, y) == 1
+
 def is_carmichael_number(n):
-    if n < 561:
+    if n < 2:
         return False
     
     for a in range(2, n):
-        if gcd(a, n) == 1 and pow(a, n-1, n) != 1:
+        if is_coprime(a, n) and pow(a, n-1, n) != 1:
             return False
     
     return True
 
+# Input number
 num = int(input("Enter a number to check if it is a Carmichael number: "))
 
 if is_carmichael_number(num):
