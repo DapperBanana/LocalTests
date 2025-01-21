f5cab5fe50e9cab7c5e6661e0f1c6136dd4cd6cb
Austin L. Howard <austin.lane.howard@gmail.com>
2025-01-21 13:23:59 -0700

    Removed unused assets from the project

--- sandbox_test.py
+++ sandbox_test.py
@@ -1,46 +1,18 @@
 
-import random
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
 
-player_hp = 100
-enemy_hp = 100
+def lcm(a, b):
+    return a * b // gcd(a, b)
 
-def player_attack():
-    damage = random.randint(10, 20)
-    return damage
+def lcm_list(numbers):
+    result = numbers[0]
+    for num in numbers[1:]:
+        result = lcm(result, num)
+    return result
 
-def enemy_attack():
-    damage = random.randint(5, 15)
-    return damage
-
-def battle():
-    global player_hp
-    global enemy_hp
-    print("A wild enemy appears!")
-    
-    while player_hp > 0 and enemy_hp > 0:
-        print("Player HP:", player_hp)
-        print("Enemy HP:", enemy_hp)
-        player_action = input("Choose an action: (1) Attack\n")
-        
-        if player_action == '1':
-            player_damage = player_attack()
-            print("Player attacks and deals", player_damage, "damage")
-            enemy_hp -= player_damage
-            
-            if enemy_hp <= 0:
-                print("Player wins!")
-                break
-            
-            enemy_damage = enemy_attack()
-            print("Enemy attacks and deals", enemy_damage, "damage")
-            player_hp -= enemy_damage
-            
-            if player_hp <= 0:
-                print("Enemy wins!")
-                break
-            
-        else:
-            print("Invalid action. Please choose again.")
-            
-
-battle()
+numbers = [2, 3, 5, 7]  # List of numbers for which LCM needs to be calculated
+result = lcm_list(numbers)
+print(f"The LCM of {numbers} is: {result}")
