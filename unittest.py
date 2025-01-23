143f59e1c133a7c6fa56170b8656b77082429855
Austin L. Howard <austin.lane.howard@gmail.com>
2025-01-23 11:45:30 -0700

    Removed unnecessary debug statements

---
+++ unittest.py
@@ -0,0 +1,10 @@
+
+from fractions import Fraction
+
+def decimal_to_fraction(decimal):
+    return Fraction(decimal).limit_denominator()
+
+decimal = float(input("Enter a decimal number: "))
+fraction = decimal_to_fraction(decimal)
+
+print(f"The fraction representation of {decimal} is: {fraction}")
