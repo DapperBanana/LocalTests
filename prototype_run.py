efff38c9dfe0cda5fed0b8bcb6ad2520937e1602
Austin L. Howard <austin.lane.howard@gmail.com>
2025-01-24 13:58:48 -0700

    Fixed accessibility issues in the UI

---
+++ prototype_run.py
@@ -0,0 +1,14 @@
+
+# Function to sort a list of integers in ascending order
+def sort_list_ascending(input_list):
+    input_list.sort()
+    return input_list
+
+# Input list of integers
+input_list = [5, 2, 8, 3, 1]
+
+# Sort the input list in ascending order
+sorted_list = sort_list_ascending(input_list)
+
+# Display the sorted list
+print("Sorted List in Ascending Order:", sorted_list)
