03dbf5640add1df24ffe1c468cbd51584514c720
Austin L. Howard <austin.lane.howard@gmail.com>
2025-01-24 13:58:41 -0700

    Fixed issue with file uploads in feature II

---
+++ safety_version.py
@@ -0,0 +1,8 @@
+
+def convert_to_title_case(string):
+    return string.title()
+
+# Test the program
+input_string = "hello world"
+output_string = convert_to_title_case(input_string)
+print(output_string)
