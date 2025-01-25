36a836eefbec3cba6d1b2f1d31f588338f0632f7
Austin L. Howard <austin.lane.howard@gmail.com>
2025-01-25 13:05:14 -0700

    Updated documentation for function X

--- verification_test.py
+++
@@ -1,8 +0,0 @@
-
-def reverse_string(input_string):
-    return input_string[::-1]
-
-if __name__ == "__main__":
-    user_input = input("Enter a string to reverse: ")
-    reversed_string = reverse_string(user_input)
-    print(f"The reversed string is: {reversed_string}")
