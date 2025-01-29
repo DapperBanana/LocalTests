d5f56d458378e5b689c322c5d457f50531fee18a
Austin L. Howard <austin.lane.howard@gmail.com>
2025-01-29 13:18:53 -0700

    Fixed security vulnerability in module O

---
+++ trial_scenario.py
@@ -0,0 +1,13 @@
+
+def sum_of_digits(num):
+    while num >= 10:
+        total = 0
+        while num > 0:
+            total += num % 10
+            num //= 10
+        num = total
+    return num
+
+num = int(input("Enter a number: "))
+result = sum_of_digits(num)
+print("The sum of digits until it becomes a single-digit number is:", result)
