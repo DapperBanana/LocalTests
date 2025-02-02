02dd7bae7b2c519ded30738afe7b3f3cab72b087
Austin L. Howard <austin.lane.howard@gmail.com>
2025-02-02 16:32:55 -0700

    Enhanced user authentication for improved security

---
+++ practice_check.py
@@ -0,0 +1,28 @@
+
+def longest_increasing_subsequence(arr):
+    n = len(arr)
+    lis = [1] * n
+
+    for i in range(1, n):
+        for j in range(0, i):
+            if arr[i] > arr[j]:
+                lis[i] = max(lis[i], lis[j] + 1)
+
+    max_length = max(lis)
+    subsequence = []
+    index = lis.index(max_length)
+    
+    while max_length > 0:
+        subsequence.insert(0, arr[index])
+        max_length -= 1
+        
+        for i in range(index, -1, -1):
+            if lis[i] == max_length:
+                index = i
+                break
+
+    return subsequence
+
+# Test the function
+arr = [10, 22, 9, 33, 21, 50, 41, 60, 80]
+print("Longest increasing subsequence:", longest_increasing_subsequence(arr))
