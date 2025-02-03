6005fa4932030ebb9e639ac8f091124ffc1c1db5
Austin L. Howard <austin.lane.howard@gmail.com>
2025-02-03 15:40:58 -0700

    Fixed memory leaks in module KK

---
+++ baseline_code.py
@@ -0,0 +1,8 @@
+
+def convert_to_title_case(input_string):
+    title_case_string = input_string.title()
+    return title_case_string
+
+input_string = input("Enter a string: ")
+title_case_string = convert_to_title_case(input_string)
+print("String in title case:", title_case_string)
