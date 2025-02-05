ce4f5daf702172d9c82da12b9694f5783464d0f4
Austin L. Howard <austin.lane.howard@gmail.com>
2025-02-05 13:03:08 -0700

    Added new feature Z

---
+++ trial_code.py
@@ -0,0 +1,10 @@
+
+def reverse_string(input_str):
+    return input_str[::-1]
+
+# Get input from user
+input_str = input("Enter a string to reverse: ")
+
+# Call the function and print the reversed string
+reversed_str = reverse_string(input_str)
+print("Reversed string:", reversed_str)
