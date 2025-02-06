0067651ac3b2d524efbb07830d08b0051de895ab
Austin L. Howard <austin.lane.howard@gmail.com>
2025-02-06 14:01:57 -0700

    Added support for user roles in the application

---
+++ dry_code.py
@@ -0,0 +1,11 @@
+
+def convert_to_title_case(input_string):
+    return input_string.title()
+
+# take input from the user
+input_string = input("Enter a string: ")
+
+# convert the string to title case
+title_case_string = convert_to_title_case(input_string)
+
+print("String in title case: ", title_case_string)
