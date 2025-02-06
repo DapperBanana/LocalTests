90f7d9d62a4910ca5ee0e85ea22d00cd73c48496
Austin L. Howard <austin.lane.howard@gmail.com>
2025-02-06 13:57:34 -0700

    Added support for user preferences

---
+++ script_version.py
@@ -0,0 +1,9 @@
+
+def sum_list_elements(input_list):
+    total_sum = sum(input_list)
+    return total_sum
+
+# Test the program
+test_list = [1, 2, 3, 4, 5]
+result = sum_list_elements(test_list)
+print("The sum of all elements in the list is:", result)
