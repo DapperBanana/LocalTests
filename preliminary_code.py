868ab1d66dd685cf5e7209c30c6d4822d3181b10
Austin L. Howard <austin.lane.howard@gmail.com>
2025-02-08 17:19:59 -0700

    Fixed broken unit tests in module HH

---
+++ preliminary_code.py
@@ -0,0 +1,45 @@
+
+def add_matrices(matrix1, matrix2):
+    result = []
+    for i in range(len(matrix1)):
+        row = []
+        for j in range(len(matrix1[0])):
+            row.append(matrix1[i][j] + matrix2[i][j])
+        result.append(row)
+    return result
+
+def subtract_matrices(matrix1, matrix2):
+    result = []
+    for i in range(len(matrix1)):
+        row = []
+        for j in range(len(matrix1[0])):
+            row.append(matrix1[i][j] - matrix2[i][j])
+        result.append(row)
+    return result
+
+def print_matrix(matrix):
+    for row in matrix:
+        print(row)
+
+# Example matrices
+matrix1 = [[1, 2, 3],
+           [4, 5, 6],
+           [7, 8, 9]]
+
+matrix2 = [[9, 8, 7],
+           [6, 5, 4],
+           [3, 2, 1]]
+
+print("Matrix 1:")
+print_matrix(matrix1)
+
+print("\nMatrix 2:")
+print_matrix(matrix2)
+
+print("\nAddition result:")
+result_addition = add_matrices(matrix1, matrix2)
+print_matrix(result_addition)
+
+print("\nSubtraction result:")
+result_subtraction = subtract_matrices(matrix1, matrix2)
+print_matrix(result_subtraction)
