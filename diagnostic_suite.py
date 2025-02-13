fe8b2be27fc4d848f0165d7106f04801690ab1a9
Austin L. Howard <austin.lane.howard@gmail.com>
2025-02-13 15:34:58 -0700

    Updated UI components for a more modern look

---
+++ diagnostic_suite.py
@@ -0,0 +1,12 @@
+
+def convert_to_title_case(input_string):
+    return input_string.title()
+
+# Take input from the user
+input_string = input("Enter a string: ")
+
+# Convert the input string to title case
+title_case_string = convert_to_title_case(input_string)
+
+# Print the title case string
+print("Title case string:", title_case_string)
