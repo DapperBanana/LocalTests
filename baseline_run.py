642e9bad6701161eff115c3bbd7bebb90e85bf93
Austin L. Howard <austin.lane.howard@gmail.com>
2025-02-14 11:30:14 -0700

    Refined data validation for input M

---
+++ baseline_run.py
@@ -0,0 +1,9 @@
+
+def calculate_cube_surface_area(side_length):
+    surface_area = 6 * side_length ** 2
+    return surface_area
+
+side_length = float(input("Enter the length of the side of the cube: "))
+surface_area = calculate_cube_surface_area(side_length)
+
+print(f"The surface area of the cube with side length {side_length} is {surface_area}")
