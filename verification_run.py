eb026349b8e63fcffe23e58be409af2731abbb93
Austin L. Howard <austin.lane.howard@gmail.com>
2025-02-18 14:43:58 -0700

    Resolved cross-browser compatibility issues

---
+++ verification_run.py
@@ -0,0 +1,11 @@
+
+# List of integers to be sorted
+numbers = [5, 2, 8, 3, 1, 9, 4]
+
+# Sorting the list in ascending order
+sorted_numbers = sorted(numbers)
+
+# Printing the sorted list
+print("Sorted numbers in ascending order:")
+for num in sorted_numbers:
+    print(num)
