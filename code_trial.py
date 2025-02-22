e828cf40a8de4ce6327cac8224254bb86e5800c0
Austin L. Howard <austin.lane.howard@gmail.com>
2025-02-21 18:45:23 -0700

    Integrated feedback from code review

--- code_trial.py
+++
@@ -1,8 +0,0 @@
-
-def title_case(s):
-    return s.title()
-
-# Test the function
-input_string = "hello world"
-output_string = title_case(input_string)
-print(output_string)
--- test_suite.py
+++ test_suite.py
@@ -1,13 +1,27 @@
 
-import re
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
 
-def extract_hashtags(text):
-    hashtags = re.findall(r"#(\w+)", text)
-    return hashtags
+    while low <= high:
+        mid = (low + high) // 2
+        mid_val = arr[mid]
 
-text = "Check out this #beautiful sunset! #nature #photography #nofilter"
-hashtags = extract_hashtags(text)
+        if mid_val == target:
+            return mid
+        elif mid_val < target:
+            low = mid + 1
+        else:
+            high = mid - 1
 
-print("Hashtags found in the text:")
-for hashtag in hashtags:
-    print("#" + hashtag)
+    return -1
+
+# Test the binary search function
+arr = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+target = 13
+result = binary_search(arr, target)
+
+if result != -1:
+    print(f"Target {target} found at index {result}")
+else:
+    print(f"Target {target} not found in the list")
