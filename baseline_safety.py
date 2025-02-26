72612b3a61d446a58475245f63c2631801d7d1ff
Austin L. Howard <austin.lane.howard@gmail.com>
2025-02-26 11:54:22 -0700

    Removed unnecessary debug statements

---
+++ baseline_safety.py
@@ -0,0 +1,9 @@
+
+# Input list of integers
+numbers = [5, 2, 8, 12, 3, 7]
+
+# Sorting the list in ascending order
+sorted_numbers = sorted(numbers)
+
+# Output the sorted list
+print("Sorted list in ascending order:", sorted_numbers)
