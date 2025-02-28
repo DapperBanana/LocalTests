8c12a446a4918c25412e3686a6ae9b2afcca0fcd
Austin L. Howard <austin.lane.howard@gmail.com>
2025-02-27 18:19:12 -0700

    Adjusted API response format for better compatibility

---
+++ initial_check.py
@@ -0,0 +1,10 @@
+
+def sum_list(lst):
+    total = 0
+    for num in lst:
+        total += num
+    return total
+
+# Test the function
+my_list = [1, 2, 3, 4, 5]
+print("Sum of elements in the list:", sum_list(my_list))
