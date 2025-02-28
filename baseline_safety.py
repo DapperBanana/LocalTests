4356a6c8c0d952525037e4a893f359265f8cd523
Austin L. Howard <austin.lane.howard@gmail.com>
2025-02-27 18:12:32 -0700

    Resolved cross-browser compatibility issues

--- baseline_safety.py
+++
@@ -1,9 +0,0 @@
-
-# Input list of integers
-numbers = [5, 2, 8, 12, 3, 7]
-
-# Sorting the list in ascending order
-sorted_numbers = sorted(numbers)
-
-# Output the sorted list
-print("Sorted list in ascending order:", sorted_numbers)
