40ad6e430c5a1bf97f3aa72581740e59dcbb73d9
Austin L. Howard <austin.lane.howard@gmail.com>
2025-03-02 13:13:52 -0700

    Applied coding standards to maintain consistency

---
+++ code_trial_run.py
@@ -0,0 +1,22 @@
+
+def romanToInt(s):
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    
+    prev_value = 0
+    for char in s:
+        value = roman_dict[char]
+        if value > prev_value:
+            result += value - 2 * prev_value
+        else:
+            result += value
+        prev_value = value
+    
+    return result
+
+# Test the function
+print(romanToInt('III'))  # Output: 3
+print(romanToInt('IV'))   # Output: 4
+print(romanToInt('IX'))   # Output: 9
+print(romanToInt('LVIII'))  # Output: 58
+print(romanToInt('MCMXCIV'))  # Output: 1994
