4333f41e538d5dc07d049cff74753fd37c875dca
Austin L. Howard <austin.lane.howard@gmail.com>
2025-03-06 15:28:12 -0700

    Enhanced API error responses for clients

---
+++ practice_run.py
@@ -0,0 +1,12 @@
+
+def sort_list_ascending(lst):
+    return sorted(lst)
+
+# Input list of integers
+lst = [5, 2, 8, 1, 9]
+
+# Sort the list in ascending order
+sorted_lst = sort_list_ascending(lst)
+
+# Print the sorted list
+print("Sorted list in ascending order:", sorted_lst)
