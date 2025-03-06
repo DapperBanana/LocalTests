4aae5a8d64a4c016fa9e8f7140a2fc3003da1640
Austin L. Howard <austin.lane.howard@gmail.com>
2025-03-06 15:16:32 -0700

    Integrated feedback from usability testing

---
+++ script_check.py
@@ -0,0 +1,15 @@
+
+def decimal_to_binary(decimal):
+    if decimal == 0:
+        return "0"
+    
+    binary = ""
+    while decimal > 0:
+        binary = str(decimal % 2) + binary
+        decimal = decimal // 2
+    
+    return binary
+
+decimal_number = int(input("Enter a decimal number: "))
+binary_number = decimal_to_binary(decimal_number)
+print(f"The binary representation of {decimal_number} is {binary_number}")
