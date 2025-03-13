0197286d11dd7b520217820dec9824bb3a761ba9
Austin L. Howard <austin.lane.howard@gmail.com>
2025-03-13 14:26:25 -0600

    Removed unused assets from the project

---
+++ trial_diagnostic.py
@@ -0,0 +1,11 @@
+
+# List of integers to be sorted
+numbers = [5, 2, 8, 1, 3, 10, 4, 6]
+
+# Sort the list in ascending order
+sorted_numbers = sorted(numbers)
+
+# Print the sorted list
+print("Sorted numbers in ascending order:")
+for num in sorted_numbers:
+    print(num, end=" ")
