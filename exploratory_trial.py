1a1ef3e8240614e0f80b87f59f1823e176212231
Austin L. Howard <austin.lane.howard@gmail.com>
2025-03-16 15:08:13 -0600

    Enhanced error messages for better user understanding

---
+++ exploratory_trial.py
@@ -0,0 +1,9 @@
+
+def convert_to_title_case(input_string):
+    return input_string.title()
+
+input_string = "hello world"
+title_case_string = convert_to_title_case(input_string)
+
+print("Original String:", input_string)
+print("Title Case String:", title_case_string)
