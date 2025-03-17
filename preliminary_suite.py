13a8978f9a6318f5583ae09d3dbf7432b0d74b96
Austin L. Howard <austin.lane.howard@gmail.com>
2025-03-17 12:53:10 -0600

    Fixed bug in module Y

--- preliminary_suite.py
+++ preliminary_suite.py
@@ -1,15 +1,19 @@
 
-def is_perfect_digital_invariant(number):
-    num_str = str(number)
-    total_sum = 0
-    
-    for digit in num_str:
-        total_sum += int(digit) ** int(digit)
-    
-    return total_sum == number
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
 
-number = int(input("Enter a number: "))
-if is_perfect_digital_invariant(number):
-    print(f"{number} is a perfect digital invariant.")
-else:
-    print(f"{number} is not a perfect digital invariant.")
+def sum_of_primes(limit):
+    prime_sum = 0
+    for i in range(2, limit + 1):
+        if is_prime(i):
+            prime_sum += i
+    return prime_sum
+
+limit = int(input("Enter the limit: "))
+result = sum_of_primes(limit)
+print("The sum of all prime numbers up to", limit, "is:", result)
