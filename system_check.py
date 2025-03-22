058620e6b98e71b56e8cc418182dc84c3ae3d3c8
Austin L. Howard <austin.lane.howard@gmail.com>
2025-03-22 14:26:09 -0600

    Removed unused assets from the project

---
+++ system_check.py
@@ -0,0 +1,7 @@
+
+def convert_to_title_case(string):
+    return string.title()
+
+input_string = input("Enter a string: ")
+title_case_string = convert_to_title_case(input_string)
+print("String in title case:", title_case_string)
