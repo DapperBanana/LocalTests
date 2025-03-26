8a976237935959f3f96ef1b5283335a69f713aa2
Austin L. Howard <austin.lane.howard@gmail.com>
2025-03-25 18:25:41 -0600

    Implemented pagination for large datasets

---
+++ safety_experiment.py
@@ -0,0 +1,11 @@
+
+def reverse_string(input_str):
+    return input_str[::-1]
+
+# Get user input
+user_input = input("Enter a string to reverse: ")
+
+# Call the reverse_string function
+reversed_str = reverse_string(user_input)
+
+print("Reversed string:", reversed_str)
