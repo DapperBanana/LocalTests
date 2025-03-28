7be2ff98d41ae7cbac25069ae7c39b7a23ee8b62
Austin L. Howard <austin.lane.howard@gmail.com>
2025-03-28 14:10:25 -0600

    Implemented automated deployment scripts

---
+++ safety_code.py
@@ -0,0 +1,8 @@
+
+def convert_to_title_case(input_string):
+    return input_string.title()
+
+# Test the function
+user_input = input("Enter a string: ")
+result = convert_to_title_case(user_input)
+print("Title case output:", result)
