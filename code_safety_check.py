c5bf2280f34c2c9b0450562fd8525e2c3cb3813e
Austin L. Howard <austin.lane.howard@gmail.com>
2025-03-31 13:56:06 -0600

    Optimized database schema for improved efficiency

---
+++ code_safety_check.py
@@ -0,0 +1,15 @@
+
+def count_characters(text):
+    char_count = {}
+    
+    for char in text:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    
+    for char, count in char_count.items():
+        print(f"{char}: {count}")
+
+text = input("Enter a string: ")
+count_characters(text)
