a0ea6477ef40e19eef375eb92bde113ccda629ca
Austin L. Howard <austin.lane.howard@gmail.com>
2025-04-02 16:50:00 -0600

    Updated project dependencies for new features

--- practice_test.py
+++
@@ -1,9 +0,0 @@
-
-def calculate_square(lst):
-    square_lst = [num**2 for num in lst]
-    return square_lst
-
-# Test the function
-my_list = [1, 2, 3, 4, 5]
-result = calculate_square(my_list)
-print(result)
