c6682783c23e2433b163302b9918d01bcd967c4c
Austin L. Howard <austin.lane.howard@gmail.com>
2025-04-05 14:39:15 -0600

    Reorganized project structure for clarity

---
+++ preliminary_probe.py
@@ -0,0 +1,21 @@
+
+def reverse_matrix(matrix):
+    reversed_matrix = []
+    for i in range(len(matrix[0])):
+        reversed_row = []
+        for j in range(len(matrix)):
+            reversed_row.append(matrix[j][i])
+        reversed_matrix.append(reversed_row)
+    return reversed_matrix
+
+# Test the function
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+reversed_matrix = reverse_matrix(matrix)
+
+for row in reversed_matrix:
+    print(row)
