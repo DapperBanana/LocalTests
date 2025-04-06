6199b4c42e0eaf9386b3e30e0f3bfc0cdfb2891f
Austin L. Howard <austin.lane.howard@gmail.com>
2025-04-06 15:01:11 -0600

    Refactored code for better scalability

---
+++ script_run.py
@@ -0,0 +1,9 @@
+
+def square_elements(lst):
+    squared_list = [num**2 for num in lst]
+    return squared_list
+
+# Example
+my_list = [1, 2, 3, 4, 5]
+result = square_elements(my_list)
+print(result)
