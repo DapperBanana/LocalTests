9ae1ca98132458cb67380f7e9b0373b5d2033b8f
Austin L. Howard <austin.lane.howard@gmail.com>
2025-04-11 13:06:36 -0600

    Reverted changes in file Q for compatibility

---
+++ diagnostic_run_check.py
@@ -0,0 +1,28 @@
+
+from xml.dom import minidom
+
+def is_valid_xml_with_dtd(xml_string):
+    try:
+        dom = minidom.parseString(xml_string)
+        if dom.doctype:
+            return True
+        return False
+    except Exception as e:
+        return False
+
+# Test the function
+xml_string = """<?xml version="1.0"?>
+<!DOCTYPE note SYSTEM "note.dtd">
+<note>
+  <to>Tove</to>
+  <from>Jani</from>
+  <heading>Reminder</heading>
+  <body>Don't forget me this weekend!</body>
+</note>
+"""
+
+if is_valid_xml_with_dtd(xml_string):
+    print("The given string is a valid XML document with a DTD.")
+else:
+    print("The given string is not a valid XML document with a DTD.")
+
