47b82eec454214769cf3deb484879074ed8d838e
Austin L. Howard <austin.lane.howard@gmail.com>
2025-04-12 12:55:29 -0600

    Documented code changes for team knowledge

--- acceptance_probe.py
+++
@@ -1,9 +0,0 @@
-
-def calculate_average(numbers):
-    total = sum(numbers)
-    average = total / len(numbers)
-    return average
-
-numbers = [10, 20, 30, 40, 50]
-average = calculate_average(numbers)
-print("Average:", average)
