de129d4e2851d266ff56ca27618502cf588acd56
Austin L. Howard <austin.lane.howard@gmail.com>
2025-04-13 16:43:55 -0600

    Refactored code for better scalability

---
+++ beta_test.py
@@ -0,0 +1,27 @@
+
+def roman_to_int(roman):
+    roman_dict = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+    
+    result = 0
+    prev_value = 0
+    
+    for letter in roman:
+        value = roman_dict[letter]
+        if value > prev_value:
+            result += value - 2 * prev_value
+        else:
+            result += value
+        prev_value = value
+        
+    return result
+
+roman_numeral = input("Enter a Roman numeral: ")
+print("Integer equivalent:", roman_to_int(roman_numeral))
