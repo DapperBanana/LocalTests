4b18ec09d84ec9a19dc0b60b42c58f291dcec65e
Austin L. Howard <austin.lane.howard@gmail.com>
2025-04-14 18:45:03 -0600

    Improved performance of image processing

--- practice_script.py
+++
@@ -1,9 +0,0 @@
-
-def square_elements(lst):
-    squared_list = [num ** 2 for num in lst]
-    return squared_list
-
-# Test the program
-numbers = [1, 2, 3, 4, 5]
-squared_numbers = square_elements(numbers)
-print(squared_numbers)
