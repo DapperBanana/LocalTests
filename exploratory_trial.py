bc9b42cb3a277110aa5be2eba8b8dd899a6bd184
Austin L. Howard <austin.lane.howard@gmail.com>
2025-04-21 12:37:36 -0600

    Renamed variable G for clarity

--- exploratory_trial.py
+++ exploratory_trial.py
@@ -1,20 +1,13 @@
 
-from scapy.all import *
+def is_leap_year(year):
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        return True
+    else:
+        return False
 
-def analyze_traffic(packet):
-    if IP in packet:
-        ip_src = packet[IP].src
-        ip_dst = packet[IP].dst
-        print(f"IP source: {ip_src} --> IP destination: {ip_dst}")
+year = int(input("Enter a year: "))
 
-    if TCP in packet:
-        tcp_sport = packet[TCP].sport
-        tcp_dport = packet[TCP].dport
-        print(f"TCP source port: {tcp_sport} --> TCP destination port: {tcp_dport}")
-
-def capture_traffic(iface):
-    sniff(iface=iface, prn=analyze_traffic)
-
-if __name__ == '__main__':
-    iface = input("Enter the interface to capture traffic on: ")
-    capture_traffic(iface)
+if is_leap_year(year):
+    print(f"{year} is a leap year.")
+else:
+    print(f"{year} is not a leap year.")
