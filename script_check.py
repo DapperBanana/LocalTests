94357305e5e4ffabe9e69a878f9533cecfef8a40
Austin L. Howard <austin.lane.howard@gmail.com>
2025-04-29 12:49:21 -0600

    Implemented user permission checks in module JJ

--- script_check.py
+++ script_check.py
@@ -1,12 +1,32 @@
 
-import math
+def is_magic_square(matrix):
+    n = len(matrix)
+    magic_num = n * (n**2 + 1) // 2
 
-def square_root(num):
-    return math.sqrt(num)
+    # Check rows and columns
+    for i in range(n):
+        row_sum = sum(matrix[i])
+        col_sum = sum(matrix[j][i] for j in range(n))
+        if row_sum != magic_num or col_sum != magic_num:
+            return False
 
-num = float(input("Enter a number: "))
-if num < 0:
-    print("Invalid input. Please enter a positive number.")
+    # Check diagonal sums
+    diag_sum1 = sum(matrix[i][i] for i in range(n))
+    diag_sum2 = sum(matrix[i][n-1-i] for i in range(n))
+    
+    if diag_sum1 != magic_num or diag_sum2 != magic_num:
+        return False
+
+    return True
+
+# Sample matrix
+matrix = [
+    [2, 7, 6],
+    [9, 5, 1],
+    [4, 3, 8]
+]
+
+if is_magic_square(matrix):
+    print("The given matrix is a magic square.")
 else:
-    result = square_root(num)
-    print(f"The square root of {num} is {result}")
+    print("The given matrix is not a magic square.")
