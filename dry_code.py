8db55f091f13484d2d2531f74ea450a3b65b3c02
Austin L. Howard <austin.lane.howard@gmail.com>
2025-05-03 11:30:40 -0600

    Implemented automated deployment scripts

---
+++ dry_code.py
@@ -0,0 +1,13 @@
+
+def find_common_elements(list1, list2):
+    common_elements = []
+    for element in list1:
+        if element in list2:
+            common_elements.append(element)
+    return common_elements
+
+list1 = [1, 2, 3, 4, 5]
+list2 = [3, 4, 5, 6, 7]
+
+common_elements = find_common_elements(list1, list2)
+print("Common elements between list1 and list2:", common_elements)
