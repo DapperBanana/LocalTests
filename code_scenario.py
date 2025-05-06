ab70ffb3cfb34003264ad60c0d1e80af08fd55e0
Austin L. Howard <austin.lane.howard@gmail.com>
2025-05-06 14:48:00 -0600

    Added support for user roles in the application

---
+++ code_scenario.py
@@ -0,0 +1,31 @@
+
+def longest_increasing_subsequence(arr):
+    n = len(arr)
+    lis = [1]*n
+    
+    for i in range(1, n):
+        for j in range(0, i):
+            if arr[i] > arr[j]:
+                lis[i] = max(lis[i], lis[j] + 1)
+    
+    max_length = max(lis)
+    subsequence = []
+    index = lis.index(max_length)
+    
+    while max_length > 0:
+        subsequence.insert(0, arr[index])
+        max_length -= 1
+        for i in range(index-1, -1, -1):
+            if lis[i] == max_length and arr[i] < arr[index]:
+                index = i
+                break
+    
+    return subsequence
+
+
+# Example
+arr = [10, 22, 9, 33, 21, 50, 41, 60, 80]
+print("Input array:", arr)
+
+result = longest_increasing_subsequence(arr)
+print("Longest increasing subsequence:", result)
