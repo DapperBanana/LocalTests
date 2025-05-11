b2619b1af53731b977c39931f24d530619d26145
Austin L. Howard <austin.lane.howard@gmail.com>
2025-05-11 16:32:24 -0600

    Implemented pagination for large datasets

---
+++ code_trial_run.py
@@ -0,0 +1,10 @@
+
+import csv
+
+# Open the CSV file
+with open('data.csv', 'r') as file:
+    csv_reader = csv.reader(file)
+    
+    # Print each row in the CSV file
+    for row in csv_reader:
+        print(row)
