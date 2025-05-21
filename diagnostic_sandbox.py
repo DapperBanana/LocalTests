66eb28976f46b678ff39973ec44b60a6fa9d70d4
Austin L. Howard <austin.lane.howard@gmail.com>
2025-05-21 16:48:33 -0600

    Changed styling for better readability

---
+++ diagnostic_sandbox.py
@@ -0,0 +1,19 @@
+
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def sum_of_primes(limit):
+    sum_primes = 0
+    for i in range(2, limit+1):
+        if is_prime(i):
+            sum_primes += i
+    return sum_primes
+
+limit = int(input("Enter the limit to calculate the sum of prime numbers: "))
+result = sum_of_primes(limit)
+print("The sum of all prime numbers up to", limit, "is:", result)
