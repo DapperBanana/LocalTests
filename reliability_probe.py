1681fcadb1253133ae083d73461bf3e5c52846d6
Austin L. Howard <austin.lane.howard@gmail.com>
2025-05-23 15:58:39 -0600

    Integrated feedback from user testing

---
+++ reliability_probe.py
@@ -0,0 +1,37 @@
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def is_empty(self):
+        return len(self.stack) == 0
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.stack.pop()
+        else:
+            return None
+
+    def peek(self):
+        if not self.is_empty():
+            return self.stack[-1]
+        else:
+            return None
+
+# Example usage
+stack = Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+
+print(stack.peek())  # Output: 3
+print(stack.pop())   # Output: 3
+print(stack.pop())   # Output: 2
+print(stack.is_empty())  # Output: False
+print(stack.pop())   # Output: 1
+print(stack.is_empty())  # Output: True
+print(stack.pop())   # Output: None
+print(stack.peek())  # Output: None
