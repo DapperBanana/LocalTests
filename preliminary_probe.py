93445dde893399d7f7e5fdf0bef715322d705e21
Austin L. Howard <austin.lane.howard@gmail.com>
2025-05-31 16:43:00 -0600

    Refactored code to follow design patterns in module BB

---
+++ preliminary_probe.py
@@ -0,0 +1,27 @@
+
+import tweepy
+from textblob import TextBlob
+
+# Twitter API credentials
+consumer_key = "YOUR_CONSUMER_KEY"
+consumer_secret = "YOUR_CONSUMER_SECRET"
+access_token = "YOUR_ACCESS_TOKEN"
+access_token_secret = "YOUR_ACCESS_TOKEN_SECRET"
+
+# Authenticate with Twitter
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+# Get tweets from a specific user
+username = "realDonaldTrump"
+tweets = api.user_timeline(screen_name=username, count=10)
+
+# Perform sentiment analysis on each tweet
+for tweet in tweets:
+    analysis = TextBlob(tweet.text)
+    
+    # Print the tweet and its sentiment
+    print(tweet.text)
+    print("Sentiment:", analysis.sentiment)
+    print()
