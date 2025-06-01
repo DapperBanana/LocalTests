6c356310890b811bbe88437c576a5796a9aab687
Austin L. Howard <austin.lane.howard@gmail.com>
2025-06-01 17:17:12 -0600

    Improved user interface for feature J

---
+++ preliminary_probe.py
@@ -0,0 +1,9 @@
+
+def reverse_string(s):
+    return s[::-1]
+
+# Get input from the user
+input_string = input("Enter a string: ")
+reversed_string = reverse_string(input_string)
+
+print("Reversed string:", reversed_string)
