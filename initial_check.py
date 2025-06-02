6579231e692cc2b1b8fabfbd40c8a7c7781bd7a4
Austin L. Howard <austin.lane.howard@gmail.com>
2025-06-02 12:55:41 -0600

    Added support for internationalization

--- initial_check.py
+++
@@ -1,9 +0,0 @@
-
-def celsius_to_fahrenheit(celsius):
-    fahrenheit = (celsius * 9/5) + 32
-    return fahrenheit
-
-celsius = float(input("Enter temperature in Celsius: "))
-fahrenheit = celsius_to_fahrenheit(celsius)
-
-print(f"{celsius} Celsius is equal to {fahrenheit} Fahrenheit")
--- preliminary_probe.py
+++
@@ -1,9 +0,0 @@
-
-def reverse_string(s):
-    return s[::-1]
-
-# Get input from the user
-input_string = input("Enter a string: ")
-reversed_string = reverse_string(input_string)
-
-print("Reversed string:", reversed_string)
