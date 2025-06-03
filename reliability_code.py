7dd6165ac08984f8532c54914b82bbb2850f86ca
Austin L. Howard <austin.lane.howard@gmail.com>
2025-06-03 16:03:56 -0600

    Added missing input validation for module DD

--- reliability_code.py
+++
@@ -1,9 +0,0 @@
-
-def decimal_to_binary(decimal):
-    binary = bin(decimal)[2:]
-    return binary
-
-decimal_number = int(input("Enter a decimal number: "))
-binary_number = decimal_to_binary(decimal_number)
-
-print(f"The binary representation of {decimal_number} is: {binary_number}")
--- reliability_probe.py
+++
@@ -1,8 +0,0 @@
-
-def sort_list(lst):
-    return sorted(lst)
-
-# Test the program
-lst = [4, 2, 7, 1, 9, 5]
-sorted_lst = sort_list(lst)
-print(sorted_lst)
