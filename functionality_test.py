b7f893456231bd7cd3d5e1e03176d4ecddc959e7
Austin L. Howard <austin.lane.howard@gmail.com>
2025-06-05 13:37:22 -0600

    Aligned variable naming conventions across the project

---
+++ functionality_test.py
@@ -0,0 +1,9 @@
+
+def reverse_words(sentence):
+    words = sentence.split()
+    reversed_sentence = ' '.join(reversed(words))
+    return reversed_sentence
+
+sentence = "Hello world, how are you?"
+reversed_sentence = reverse_words(sentence)
+print(reversed_sentence)
