eadaee6734ffe87b679bc06006e94b27f34517be
Austin L. Howard <austin.lane.howard@gmail.com>
2025-06-05 13:23:21 -0600

    Changed styling for better readability

---
+++ beta_check.py
@@ -0,0 +1,11 @@
+
+import random
+
+# List of possible weather conditions
+weather_conditions = ['sunny', 'cloudy', 'rainy', 'snowy']
+
+# Generate a random weather forecast
+forecast = random.choice(weather_conditions)
+
+# Display the forecast to the user
+print("Today's weather forecast is:", forecast)
