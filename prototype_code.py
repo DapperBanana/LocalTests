361f86e0e62fd7f58acb4d13450250d97cd1e174
Austin L. Howard <austin.lane.howard@gmail.com>
2025-06-07 12:34:58 -0600

    Upgraded project dependencies to meet security standards

---
+++ prototype_code.py
@@ -0,0 +1,53 @@
+
+def is_valid_row(board, row):
+    nums = set()
+    for i in range(9):
+        if board[row][i] in nums:
+            return False
+        nums.add(board[row][i])
+    return True
+
+def is_valid_col(board, col):
+    nums = set()
+    for i in range(9):
+        if board[i][col] in nums:
+            return False
+        nums.add(board[i][col])
+    return True
+
+def is_valid_subgrid(board, start_row, start_col):
+    nums = set()
+    for i in range(start_row, start_row + 3):
+        for j in range(start_col, start_col + 3):
+            if board[i][j] in nums:
+                return False
+            nums.add(board[i][j])
+    return True
+
+def is_valid_sudoku(board):
+    for i in range(9):
+        if not is_valid_row(board, i) or not is_valid_col(board, i):
+            return False
+    for i in range(0, 9, 3):
+        for j in range(0, 9, 3):
+            if not is_valid_subgrid(board, i, j):
+                return False
+    return True
+
+#Example Sudoku solution to test the program
+sudoku_solution = [
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 7, 9]
+]
+
+if is_valid_sudoku(sudoku_solution):
+    print("Valid Sudoku solution")
+else:
+    print("Invalid Sudoku solution")
