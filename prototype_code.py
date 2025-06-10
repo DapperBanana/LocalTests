155bf68e289a0b17f35e0a6ee84a8874bc48e44f
Austin L. Howard <austin.lane.howard@gmail.com>
2025-06-10 11:58:02 -0600

    Implemented error handling for edge case E

---
+++ prototype_code.py
@@ -0,0 +1,13 @@
+
+def reverse_string(s):
+    return s[::-1]
+
+# Get user input
+original_string = input("Enter a string: ")
+
+# Reverse the string
+reversed_string = reverse_string(original_string)
+
+# Output the reversed string
+print("Original string:", original_string)
+print("Reversed string:", reversed_string)
