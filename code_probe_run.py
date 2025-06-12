c1957f421c56f6f1d28fca4bd56be3ddef6a21e1
Austin L. Howard <austin.lane.howard@gmail.com>
2025-06-12 17:51:16 -0600

    Fixed memory leaks in module KK

---
+++ code_probe_run.py
@@ -0,0 +1,11 @@
+
+def trapezoid_area(base1, base2, height):
+    area = 0.5 * (base1 + base2) * height
+    return area
+
+base1 = float(input("Enter the length of base1: "))
+base2 = float(input("Enter the length of base2: "))
+height = float(input("Enter the height of the trapezoid: "))
+
+area = trapezoid_area(base1, base2, height)
+print("The area of the trapezoid is: ", area)
