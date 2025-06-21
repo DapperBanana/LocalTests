afce244906bda6ca3e77db764a935d5606eb7975
Austin L. Howard <austin.lane.howard@gmail.com>
2025-06-21 13:24:53 -0600

    Applied coding standards to maintain consistency

---
+++ preliminary_check.py
@@ -0,0 +1,9 @@
+
+def find_largest_element(input_list):
+    largest_element = max(input_list)
+    return largest_element
+
+# Test the function with an example list
+example_list = [3, 8, 21, 5, 12]
+largest_element = find_largest_element(example_list)
+print(f"The largest element in the list is: {largest_element}")
