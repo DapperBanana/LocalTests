a4042aaa21265f7a2969833eae126d282e5da0b6
Austin L. Howard <austin.lane.howard@gmail.com>
2025-06-21 13:19:15 -0600

    Added unit tests for module F

---
+++ trial_code.py
@@ -0,0 +1,9 @@
+
+def sort_list(arr):
+    arr.sort()  # Sort the list in-place
+    return arr
+
+# Test the function
+numbers = [4, 2, 7, 1, 9, 5]
+sorted_numbers = sort_list(numbers)
+print(sorted_numbers)
