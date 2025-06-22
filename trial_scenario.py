665719a54cba457c1a1365c27cb30878e02d3114
Austin L. Howard <austin.lane.howard@gmail.com>
2025-06-22 14:07:37 -0600

    Optimized algorithm B for better performance

--- trial_scenario.py
+++
@@ -1,9 +0,0 @@
-
-def decimal_to_binary(decimal_number):
-    binary_number = bin(decimal_number)[2:]
-    return binary_number
-
-if __name__ == "__main__":
-    decimal_number = int(input("Enter a decimal number: "))
-    binary_number = decimal_to_binary(decimal_number)
-    print(f"The binary representation of {decimal_number} is: {binary_number}")
