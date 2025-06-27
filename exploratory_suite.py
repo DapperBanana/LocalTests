546e693284b36b19e331ec90b56df9e0fb3f3f7c
Austin L. Howard <austin.lane.howard@gmail.com>
2025-06-27 17:59:20 -0600

    Fixed memory leaks in feature FF

---
+++ exploratory_suite.py
@@ -0,0 +1,31 @@
+
+def is_magic_torus(matrix):
+    n = len(matrix)
+    magic_constant = n * (n**2 + 1) / 2
+
+    # Check rows
+    for row in matrix:
+        if sum(row) != magic_constant:
+            return False
+
+    # Check columns
+    for j in range(n):
+        if sum(matrix[i][j] for i in range(n)) != magic_constant:
+            return False
+
+    # Check diagonals
+    if sum(matrix[i][i] for i in range(n)) != magic_constant:
+        return False
+
+    if sum(matrix[i][n - i - 1] for i in range(n)) != magic_constant:
+        return False
+
+    return True
+
+# Example matrix
+matrix = [[2, 7, 6], [9, 5, 1], [4, 3, 8]]
+
+if is_magic_torus(matrix):
+    print("The given matrix is a magic torus!")
+else:
+    print("The given matrix is not a magic torus.")
