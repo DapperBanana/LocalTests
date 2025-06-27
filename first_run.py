25721e8acf4375fe4e62a4dffbfcaa7d440018b9
Austin L. Howard <austin.lane.howard@gmail.com>
2025-06-27 17:55:16 -0600

    Updated project logo and branding

--- first_run.py
+++
@@ -1,9 +0,0 @@
-
-def find_union(set1, set2):
-    return set1.union(set2)
-
-set1 = {1, 2, 3, 4, 5}
-set2 = {4, 5, 6, 7, 8}
-
-union_set = find_union(set1, set2)
-print(union_set)
--- sample_code.py
+++
@@ -1,9 +0,0 @@
-
-def calculate_pyramid_area(base_area, height):
-    return (base_area + (1/2) * (base_area * height))
-
-base_area = float(input("Enter the base area of the pyramid: "))
-height = float(input("Enter the height of the pyramid: "))
-
-pyramid_area = calculate_pyramid_area(base_area, height)
-print(f"The area of the pyramid is: {pyramid_area}")
