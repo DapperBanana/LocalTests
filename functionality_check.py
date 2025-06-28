883e8e999ca4b15f299ca3e5565a8cc99dcbf456
Austin L. Howard <austin.lane.howard@gmail.com>
2025-06-28 12:43:28 -0600

    Updated dependencies to version C

---
+++ functionality_check.py
@@ -0,0 +1,21 @@
+
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+def is_carmichael_number(n):
+    if n < 2 or n == 2 or n == 3:
+        return False
+    for a in range(2, n):
+        if gcd(a, n) == 1 and pow(a, n-1, n) != 1:
+            return False
+    return True
+
+# Input number to check
+num = int(input("Enter a number to check if it is a Carmichael number: "))
+
+if is_carmichael_number(num):
+    print(f"{num} is a Carmichael number.")
+else:
+    print(f"{num} is not a Carmichael number.")
