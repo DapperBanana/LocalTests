012b9fbe74aaa585d00d364276c3105c608c6ef1
Austin L. Howard <austin.lane.howard@gmail.com>
2025-07-02 14:17:11 -0600

    Implemented new endpoint in API K

---
+++ alpha_version.py
@@ -0,0 +1,18 @@
+
+def is_perfect_digital_invariant(num):
+    num_str = str(num)
+    
+    total = 0
+    for digit in num_str:
+        total += int(digit)
+    
+    if total * int(num_str[-1]) == num:
+        return True
+    else:
+        return False
+
+num = int(input("Enter a number: "))
+if is_perfect_digital_invariant(num):
+    print(f"{num} is a perfect digital invariant.")
+else:
+    print(f"{num} is not a perfect digital invariant.")
