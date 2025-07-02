7b4ccb42484b88f76e35c6087520b322a9afdc51
Austin L. Howard <austin.lane.howard@gmail.com>
2025-07-02 14:19:44 -0600

    Fixed race condition in asynchronous tasks

---
+++ code_probe_run.py
@@ -0,0 +1,18 @@
+
+def reverse_matrix(matrix):
+    reversed_matrix = []
+    for i in range(len(matrix[0])):
+        reversed_matrix.append([row[i] for row in matrix])
+    return reversed_matrix
+
+# Example
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+reversed_matrix = reverse_matrix(matrix)
+
+for row in reversed_matrix:
+    print(row)
