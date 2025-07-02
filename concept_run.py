37c4cc053d1edbd05254215d6a6bd0941621d90a
Austin L. Howard <austin.lane.howard@gmail.com>
2025-07-02 14:22:05 -0600

    Implemented error handling for edge case E

--- concept_run.py
+++ concept_run.py
@@ -1,12 +1,42 @@
 
-def find_replace_word(file_name, word_to_find, word_to_replace):
-    with open(file_name, 'r') as file:
-        file_content = file.read()
+import random
+import time
 
-    new_content = file_content.replace(word_to_find, word_to_replace)
+class TrafficSimulation:
+    def __init__(self, road_length, car_density, max_speed):
+        self.road_length = road_length
+        self.car_density = car_density
+        self.max_speed = max_speed
+        self.road = ['_' for _ in range(road_length)]
+        self.cars = []
 
-    with open(file_name, 'w') as file:
-        file.write(new_content)
+        for i in range(road_length):
+            if random.random() < car_density:
+                speed = random.randint(1, max_speed)
+                self.cars.append((i, speed))
 
-# Replace 'old_word' with 'new_word' in 'sample.txt' file
-find_replace_word('sample.txt', 'old_word', 'new_word')
+    def update(self):
+        new_cars = []
+        for i, speed in self.cars:
+            new_speed = min(speed + 1, self.max_speed)
+            new_cars.append((i + new_speed, new_speed))
+
+        self.road = ['_' for _ in range(self.road_length)]
+        for i, speed in new_cars:
+            if i < self.road_length:
+                self.road[i] = '#'
+
+        self.cars = [(i, speed) for i, speed in new_cars if i < self.road_length]
+
+    def display(self):
+        print(''.join(self.road))
+
+road_length = 50
+car_density = 0.3
+max_speed = 5
+sim = TrafficSimulation(road_length, car_density, max_speed)
+
+while True:
+    sim.update()
+    sim.display()
+    time.sleep(0.5)
