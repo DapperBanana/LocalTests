87234a683aa3088c593c2b392926374b4b3dff70
Austin L. Howard <austin.lane.howard@gmail.com>
2025-07-03 14:26:57 -0600

    Added support for dark mode in the UI

---
+++ exploratory_check.py
@@ -0,0 +1,21 @@
+
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def count_prime_numbers(start, end):
+    count = 0
+    for num in range(start, end + 1):
+        if is_prime(num):
+            count += 1
+    return count
+
+start_range = int(input("Enter the start of the range: "))
+end_range = int(input("Enter the end of the range: "))
+
+num_primes = count_prime_numbers(start_range, end_range)
+print(f'The number of prime numbers in the range {start_range}-{end_range} is: {num_primes}')
