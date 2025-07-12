774786e885dc22e9d4616860e14d6cc347b26670
Austin L. Howard <austin.lane.howard@gmail.com>
2025-07-12 12:37:32 -0600

    Reformatted code for consistency

---
+++ preliminary_validation.py
@@ -0,0 +1,55 @@
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=' ')
+            current = current.next
+        print()
+
+    def remove(self, key):
+        current = self.head
+        if current and current.data == key:
+            self.head = current.next
+            current = None
+            return
+        prev = None
+        while current and current.data != key:
+            prev = current
+            current = current.next
+        if current is None:
+            return
+        prev.next = current.next
+        current = None
+
+# Create a linked list and perform operations
+linked_list = LinkedList()
+linked_list.append(1)
+linked_list.append(2)
+linked_list.append(3)
+
+linked_list.display()
+# Output: 1 2 3
+
+linked_list.remove(2)
+
+linked_list.display()
+# Output: 1 3
