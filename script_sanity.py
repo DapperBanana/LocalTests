50736ebc0c938327134ac9ec46d7a203232b88a3
Austin L. Howard <austin.lane.howard@gmail.com>
2025-07-14 17:42:44 -0600

    Fixed memory leaks in module KK

---
+++ script_sanity.py
@@ -0,0 +1,20 @@
+
+import qrcode
+
+# Get input from user
+data = input("Enter the data for QR code: ")
+
+# Generate QR code
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data(data)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("qrcode.png")
+
+print("QR code generated successfully!")
--- trial_version.py
+++
@@ -1,7 +0,0 @@
-
-def surface_area_cube(side_length):
-    surface_area = 6 * side_length ** 2
-    return surface_area
-
-side_length = float(input("Enter the side length of the cube: "))
-print("The surface area of the cube is:", surface_area_cube(side_length))
