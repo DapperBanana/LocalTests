372fd95f927e2ebf806471a8a60b4b0e79153185
Austin L. Howard <austin.lane.howard@gmail.com>
2025-07-19 14:33:08 -0600

    Fixed typos in comments and documentation

---
+++ verification_probe.py
@@ -0,0 +1,12 @@
+
+import math
+
+def cylinder_volume(radius, height):
+    volume = math.pi * radius**2 * height
+    return volume
+
+radius = float(input("Enter the radius of the cylinder: "))
+height = float(input("Enter the height of the cylinder: "))
+
+volume = cylinder_volume(radius, height)
+print(f"The volume of the cylinder with radius {radius} and height {height} is: {volume}")
