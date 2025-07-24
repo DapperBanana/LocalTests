92c44f709deee7a7f23e1d4d2dc7d76ccc1042d8
Austin L. Howard <austin.lane.howard@gmail.com>
2025-07-24 15:14:41 -0600

    Implemented pagination for large datasets

--- verification_script.py
+++ verification_script.py
@@ -1,13 +1,10 @@
 
-def find_common_elements(list1, list2):
-    common_elements = []
-    for element in list1:
-        if element in list2 and element not in common_elements:
-            common_elements.append(element)
-    return common_elements
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
 
-list1 = [1, 2, 3, 4, 5]
-list2 = [4, 5, 6, 7, 8]
-
-common_elements = find_common_elements(list1, list2)
-print("Common elements between list1 and list2:", common_elements)
+num = int(input("Enter a number: "))
+result = factorial(num)
+print(f"The factorial of {num} is {result}")
