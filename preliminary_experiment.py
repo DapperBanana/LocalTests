d89895e6925f2162c1157bfe189b2f7b994f2afd
Austin L. Howard <austin.lane.howard@gmail.com>
2025-07-31 19:20:52 -0600

    Fixed edge case scenario in module U

--- preliminary_experiment.py
+++ preliminary_experiment.py
@@ -1,35 +1,18 @@
 
-def is_prime(n):
-    if n < 2:
-        return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
-
-def gcd(a, b):
-    while b:
-        a, b = b, a % b
-    return a
-
-def lucas_number(n):
-    return (2 ** n) - 1
-
-def is_lucas_carmichael_number(n):
-    if n < 2:
+def is_leap_year(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
         return False
 
-    if not is_prime(n):
-        return False
-
-    for i in range(2, n):
-        if (2 ** i) % n == 1 and gcd(i, n) == 1:
-            return False
-
-    return True
-
-number = int(input("Enter a number: "))
-if is_lucas_carmichael_number(number):
-    print(f"{number} is a Lucas-Carmichael number.")
+year = int(input("Enter a year: "))
+if is_leap_year(year):
+    print(year, "is a leap year.")
 else:
-    print(f"{number} is not a Lucas-Carmichael number.")
+    print(year, "is not a leap year.")
