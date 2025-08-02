7f0abc01a665e08498070e83e3f4383006e91100
Austin L. Howard <austin.lane.howard@gmail.com>
2025-08-02 17:14:18 -0600

    Optimized database queries in module V

---
+++ safety_version.py
@@ -0,0 +1,18 @@
+
+# Swapping values of two variables without using a temporary variable
+
+a = 5
+b = 10
+
+print("Before swapping:")
+print("a =", a)
+print("b =", b)
+
+# Swapping
+a = a + b
+b = a - b
+a = a - b
+
+print("\nAfter swapping:")
+print("a =", a)
+print("b =", b)
