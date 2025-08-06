3f82804d64d28c85d7975ed84ef7351d359314b8
Austin L. Howard <austin.lane.howard@gmail.com>
2025-08-06 17:30:03 -0600

    Added missing error handling in module L

--- alpha_check.py
+++
@@ -1,9 +0,0 @@
-
-def intersection(list1, list2):
-    return list(set(list1) & set(list2))
-
-list1 = [1, 2, 3, 4, 5]
-list2 = [3, 4, 5, 6, 7]
-
-intersection_list = intersection(list1, list2)
-print("Intersection of list1 and list2 is:", intersection_list)
