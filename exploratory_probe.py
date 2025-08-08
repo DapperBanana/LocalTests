1db8507cd86841e960f29e4cdc769896e9860586
Austin L. Howard <austin.lane.howard@gmail.com>
2025-08-07 19:10:51 -0600

    Optimized SQL queries for faster execution

---
+++ exploratory_probe.py
@@ -0,0 +1,12 @@
+
+def convert_to_title_case(s):
+    return s.title()
+
+# Take input from user
+user_input = input("Enter a string: ")
+
+# Convert the user input to title case
+title_case_string = convert_to_title_case(user_input)
+
+# Display the title cased string
+print("Title cased string:", title_case_string)
