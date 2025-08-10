e52169bb910e3387c3429d178db68ccf509f6784
Austin L. Howard <austin.lane.howard@gmail.com>
2025-08-09 19:10:37 -0600

    Integrated code coverage analysis tools

---
+++ practice_test.py
@@ -0,0 +1,11 @@
+
+def reverse_string(s):
+    return s[::-1]
+
+# Get user input
+string = input("Enter a string: ")
+
+# Reverse the string
+reversed_string = reverse_string(string)
+
+print("Reversed string:", reversed_string)
