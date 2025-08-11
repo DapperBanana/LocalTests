8bc4c162ab7d714421658ffae7180e0f81525c88
Austin L. Howard <austin.lane.howard@gmail.com>
2025-08-11 11:49:46 -0600

    Updated project configuration files

---
+++ validation.py
@@ -0,0 +1,28 @@
+
+def roman_to_int(roman_numeral):
+    roman_dict = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    result = 0
+    prev_value = 0
+
+    for char in roman_numeral:
+        value = roman_dict[char]
+        if value > prev_value:
+            result += value - 2*prev_value
+        else:
+            result += value
+
+        prev_value = value
+
+    return result
+
+roman_numeral = input("Enter a Roman numeral: ")
+print(f"The integer value of {roman_numeral} is: {roman_to_int(roman_numeral)}")
