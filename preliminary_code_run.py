e2bd6e91387a45ad225062181ed077aaf1942266
Austin L. Howard <austin.lane.howard@gmail.com>
2025-08-22 13:51:31 -0600

    Updated dependencies to version C

---
+++ preliminary_code_run.py
@@ -0,0 +1,33 @@
+
+import math
+
+def find_roots(a, b, c):
+    discriminant = b**2 - 4*a*c
+    if discriminant > 0:
+        root1 = (-b + math.sqrt(discriminant)) / (2*a)
+        root2 = (-b - math.sqrt(discriminant)) / (2*a)
+        return root1, root2
+    elif discriminant == 0:
+        root = -b / (2*a)
+        return root, root
+    else:
+        real_part = -b / (2*a)
+        imaginary_part = math.sqrt(abs(discriminant)) / (2*a)
+        root1 = complex(real_part, imaginary_part)
+        root2 = complex(real_part, -imaginary_part)
+        return root1, root2
+
+a = float(input("Enter the coefficient of x^2: "))
+b = float(input("Enter the coefficient of x: "))
+c = float(input("Enter the constant term: "))
+
+roots = find_roots(a, b, c)
+
+if isinstance(roots[0], complex):
+    print("The roots are complex numbers:")
+    print("Root 1:", roots[0])
+    print("Root 2:", roots[1])
+else:
+    print("The roots are real numbers:")
+    print("Root 1:", roots[0])
+    print("Root 2:", roots[1])
