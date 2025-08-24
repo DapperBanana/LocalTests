293a86f08970705f65d6bbb74ac5f9a69da4af81
Austin L. Howard <austin.lane.howard@gmail.com>
2025-08-24 15:25:18 -0600

    Optimized rendering performance in the UI

---
+++ alpha_experiment.py
@@ -0,0 +1,35 @@
+
+def is_symmetric(matrix):
+    if len(matrix) != len(matrix[0]):
+        return False
+    
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i+1, n):
+            if matrix[i][j] != matrix[j][i]:
+                return False
+    
+    return True
+
+# Example matrix
+matrix1 = [
+    [1, 2, 3],
+    [2, 4, 5],
+    [3, 5, 6]
+]
+
+matrix2 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+if is_symmetric(matrix1):
+    print("Matrix 1 is symmetric.")
+else:
+    print("Matrix 1 is not symmetric.")
+
+if is_symmetric(matrix2):
+    print("Matrix 2 is symmetric.")
+else:
+    print("Matrix 2 is not symmetric.")
