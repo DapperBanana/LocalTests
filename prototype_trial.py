f7921802a9365fb1b0277668f8dc0f59cd6b5451
Austin L. Howard <austin.lane.howard@gmail.com>
2025-08-26 17:51:11 -0600

    Fixed memory leaks in module KK

--- prototype_trial.py
+++
@@ -1,9 +0,0 @@
-
-def square_elements(lst):
-    squared_list = [num ** 2 for num in lst]
-    return squared_list
-
-# Test the function
-numbers = [1, 2, 3, 4, 5]
-result = square_elements(numbers)
-print(result)
