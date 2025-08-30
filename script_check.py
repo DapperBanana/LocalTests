d2f0a3de851d75513dfa3e810379238958106999
Austin L. Howard <austin.lane.howard@gmail.com>
2025-08-30 14:38:57 -0600

    Updated documentation for function X

---
+++ script_check.py
@@ -0,0 +1,14 @@
+
+def longest_word_length(sentence):
+    words = sentence.split()
+    max_length = 0
+    
+    for word in words:
+        if len(word) > max_length:
+            max_length = len(word)
+    
+    return max_length
+
+sentence = input("Enter a sentence: ")
+length = longest_word_length(sentence)
+print(f"The length of the longest word in the sentence is: {length}")
