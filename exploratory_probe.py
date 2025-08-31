9728ef7c6752fcb4a8ceb76dfc1683f059178abd
Austin L. Howard <austin.lane.howard@gmail.com>
2025-08-31 13:42:35 -0600

    Aligned variable naming conventions across the project

---
+++ exploratory_probe.py
@@ -0,0 +1,9 @@
+
+def square_elements(lst):
+    squared_list = [x**2 for x in lst]
+    return squared_list
+
+# Example
+numbers = [1, 2, 3, 4, 5]
+squared_numbers = square_elements(numbers)
+print(squared_numbers)
