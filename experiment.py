edd4268650a021b196e6f412930ff9901979a574
Austin L. Howard <austin.lane.howard@gmail.com>
2025-09-02 16:03:07 -0600

    Fixed UI alignment issues in feature CC

--- experiment.py
+++
@@ -1,9 +0,0 @@
-
-def sort_list(lst):
-    return sorted(lst)
-
-# Test the function
-lst = [5, 2, 8, 1, 7]
-sorted_lst = sort_list(lst)
-print("Original List:", lst)
-print("Sorted List:", sorted_lst)
