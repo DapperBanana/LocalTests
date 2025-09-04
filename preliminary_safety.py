2cae058e109e98f7f41740a771bdc380c374c7ee
Austin L. Howard <austin.lane.howard@gmail.com>
2025-09-04 12:33:02 -0600

    Optimized network requests for efficiency

---
+++ preliminary_safety.py
@@ -0,0 +1,27 @@
+
+class Question:
+    def __init__(self, prompt, answer):
+        self.prompt = prompt
+        self.answer = answer
+
+question_prompts = [
+    "What is the capital of France?\n(a) London\n(b) Paris\n(c) Berlin\n",
+    "What is the largest planet in our solar system?\n(a) Earth\n(b) Jupiter\n(c) Mars\n",
+    "What is the powerhouse of the cell?\n(a) Nucleus\n(b) Mitochondria\n(c) Ribosome\n"
+]
+
+questions = [
+    Question(question_prompts[0], "b"),
+    Question(question_prompts[1], "b"),
+    Question(question_prompts[2], "b")
+]
+
+def run_quiz(questions):
+    score = 0
+    for question in questions:
+        answer = input(question.prompt)
+        if answer == question.answer:
+            score += 1
+    print("You got " + str(score) + "/" + str(len(questions)) + " correct!")
+
+run_quiz(questions)
