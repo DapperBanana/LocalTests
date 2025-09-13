d2328816c0d9a89a5347aa8bef499f3bace3a8df
Austin L. Howard <austin.lane.howard@gmail.com>
2025-09-13 12:54:25 -0600

    Removed unused assets from the project

---
+++ diagnostic_test.py
@@ -0,0 +1,12 @@
+
+def reverse_string(input_str):
+    return input_str[::-1]
+
+# Get input from user
+original_str = input("Enter a string: ")
+
+# Reverse the string
+reversed_str = reverse_string(original_str)
+
+# Output the reversed string
+print("Reversed string:", reversed_str)
