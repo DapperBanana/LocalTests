5a4816087025c25ce08518eb9ddb679ea74e0385
Austin L. Howard <austin.lane.howard@gmail.com>
2025-09-18 14:18:26 -0600

    Removed unused assets from the project

---
+++ script_sanity.py
@@ -0,0 +1,12 @@
+
+import math
+
+def calculate_cone_volume(radius, height):
+    volume = (1/3) * math.pi * radius**2 * height
+    return volume
+
+radius = float(input("Enter the radius of the cone: "))
+height = float(input("Enter the height of the cone: "))
+
+volume = calculate_cone_volume(radius, height)
+print(f"The volume of the cone with radius {radius} and height {height} is: {volume}")
