f3bbf2650376ac8f829dcebbe80870e4878775d7
Austin L. Howard <austin.lane.howard@gmail.com>
2025-09-19 18:30:16 -0600

    Improved logging for better debugging

---
+++ initial_run.py
@@ -0,0 +1,11 @@
+
+# List of integers to be sorted
+numbers = [10, 5, 3, 8, 1, 6]
+
+# Sort the list in ascending order
+sorted_numbers = sorted(numbers)
+
+# Print the sorted list
+print("Sorted numbers in ascending order:")
+for num in sorted_numbers:
+    print(num)
