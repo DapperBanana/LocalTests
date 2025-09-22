0ac3d45031b7f0d751bdab84f76952413933d939
Austin L. Howard <austin.lane.howard@gmail.com>
2025-09-22 17:47:16 -0600

    Refactored code to follow design patterns in module BB

---
+++ safety_version.py
@@ -0,0 +1,19 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Number of steps in the random walk
+num_steps = 1000
+
+# Generate random steps
+steps = np.random.choice([-1, 1], size=num_steps)
+walk = np.cumsum(steps)
+
+# Plot the random walk
+plt.figure(figsize=(10, 6))
+plt.plot(walk)
+plt.title('Random Walk Plot')
+plt.xlabel('Steps')
+plt.ylabel('Position')
+plt.grid(True)
+plt.show()
