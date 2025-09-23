b75dcc4497229714426abac015de415c58fa01f0
Austin L. Howard <austin.lane.howard@gmail.com>
2025-09-23 11:57:25 -0600

    Refactored code for better scalability

---
+++ initial_check.py
@@ -0,0 +1,9 @@
+
+def reverse_string(input_string):
+    return input_string[::-1]
+
+# Test the program
+input_string = "hello world"
+reversed_string = reverse_string(input_string)
+print("Original String:", input_string)
+print("Reversed String:", reversed_string)
