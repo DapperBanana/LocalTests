0554f299c4db6cb97d294a32a51f2006f797643e
Austin L. Howard <austin.lane.howard@gmail.com>
2025-09-23 11:50:53 -0600

    Integrated unit test coverage for critical paths

---
+++ code_validation.py
@@ -0,0 +1,16 @@
+
+import re
+
+def is_valid_ipv4(ip):
+    pattern = "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
+    if re.match(pattern, ip):
+        return True
+    else:
+        return False
+
+ip_address = input("Enter an IPv4 address: ")
+
+if is_valid_ipv4(ip_address):
+    print(f"{ip_address} is a valid IPv4 address.")
+else:
+    print(f"{ip_address} is not a valid IPv4 address.")
