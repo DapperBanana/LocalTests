298d55267c2b510455a98b5c0faf5fb246ba5e43
Austin L. Howard <austin.lane.howard@gmail.com>
2025-09-24 11:18:31 -0600

    Integrated feedback from usability testing

--- initial_check.py
+++ initial_check.py
@@ -1,9 +1,15 @@
 
-def reverse_string(input_string):
-    return input_string[::-1]
-
-# Test the program
-input_string = "hello world"
-reversed_string = reverse_string(input_string)
-print("Original String:", input_string)
-print("Reversed String:", reversed_string)
+def count_words_in_file(file_name):
+    try:
+        with open(file_name, 'r') as file:
+            text = file.read()
+            words = text.split()
+            num_words = len(words)
+            print(f'Total number of words in the file "{file_name}" is: {num_words}')
+    except FileNotFoundError:
+        print(f'File named "{file_name}" not found.')
+    except Exception as e:
+        print(f'An error occurred: {str(e)}')
+        
+file_name = 'sample.txt'
+count_words_in_file(file_name)
