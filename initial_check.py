f69e2b41f9b72fe1b298e949473bcf4b24c1fe1a
Austin L. Howard <austin.lane.howard@gmail.com>
2025-09-24 11:22:46 -0600

    Updated project logo and branding

--- initial_check.py
+++ initial_check.py
@@ -1,15 +1,11 @@
 
-def count_words_in_file(file_name):
-    try:
-        with open(file_name, 'r') as file:
-            text = file.read()
-            words = text.split()
-            num_words = len(words)
-            print(f'Total number of words in the file "{file_name}" is: {num_words}')
-    except FileNotFoundError:
-        print(f'File named "{file_name}" not found.')
-    except Exception as e:
-        print(f'An error occurred: {str(e)}')
-        
-file_name = 'sample.txt'
-count_words_in_file(file_name)
+import random
+
+# List of possible weather conditions
+weather_conditions = ["sunny", "cloudy", "rainy", "stormy", "snowy"]
+
+# Generate a random weather forecast
+forecast = random.choice(weather_conditions)
+
+print("Today's weather forecast:")
+print(forecast)
