1c399034b8ac41d304a5d8a7ca0c48679660da64
Austin L. Howard <austin.lane.howard@gmail.com>
2025-09-26 12:02:31 -0600

    Refactored code to follow design patterns in module BB

---
+++ trial_scenario.py
@@ -0,0 +1,15 @@
+
+def common_characters(str1, str2):
+    common_chars = []
+    
+    for char in str1:
+        if char in str2 and char not in common_chars:
+            common_chars.append(char)
+    
+    return common_chars
+
+# Test the function
+str1 = "hello"
+str2 = "world"
+result = common_characters(str1, str2)
+print("Common characters:", result)
