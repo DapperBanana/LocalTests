6cf8b40dc18929d4612db1aa02bec38235a916a4
Austin L. Howard <austin.lane.howard@gmail.com>
2025-10-01 15:35:34 -0600

    Updated dependencies to version C

---
+++ script_run.py
@@ -0,0 +1,21 @@
+
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def sum_of_primes(start, end):
+    total = 0
+    for num in range(start, end + 1):
+        if is_prime(num):
+            total += num
+    return total
+
+start = int(input("Enter the start of the range: "))
+end = int(input("Enter the end of the range: "))
+
+result = sum_of_primes(start, end)
+print(f"The sum of all prime numbers between {start} and {end} is: {result}")
