6c2df36d6c375c67c142703670c649871e8c582d
Austin L. Howard <austin.lane.howard@gmail.com>
2025-10-03 16:37:48 -0600

    Implemented lazy loading for images

---
+++ scenario_test.py
@@ -0,0 +1,26 @@
+
+def longest_increasing_subsequence(arr):
+    n = len(arr)
+    lis = [1] * n
+
+    for i in range(1, n):
+        for j in range(0, i):
+            if arr[i] > arr[j] and lis[i] < lis[j] + 1:
+                lis[i] = lis[j] + 1
+
+    max_length = max(lis)
+    subsequence = []
+    index = lis.index(max_length)
+    subsequence.append(arr[index])
+
+    for i in range(index-1, -1, -1):
+        if arr[i] < arr[index] and lis[i] == lis[index] - 1:
+            subsequence.insert(0, arr[i])
+            index = i
+
+    return subsequence
+
+# Test the function
+arr = [10, 22, 9, 33, 21, 50, 41, 60, 80]
+print("Original array:", arr)
+print("Longest increasing subsequence:", longest_increasing_subsequence(arr))
