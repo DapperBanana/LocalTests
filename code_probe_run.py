b2ff8fb1983b10080c30be1652e0b61384a4f612
Austin L. Howard <austin.lane.howard@gmail.com>
2025-10-03 16:36:25 -0600

    Added support for user roles in the application

---
+++ code_probe_run.py
@@ -0,0 +1,12 @@
+
+def sort_list(input_list):
+    return sorted(input_list)
+
+# Input list of integers
+input_list = [5, 3, 8, 1, 2, 7]
+
+# Sort the list in ascending order
+sorted_list = sort_list(input_list)
+
+# Print the sorted list
+print("Sorted list in ascending order:", sorted_list)
