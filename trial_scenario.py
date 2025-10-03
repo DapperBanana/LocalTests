fbbe489624effe2e601950f2c53ce513e3fb4981
Austin L. Howard <austin.lane.howard@gmail.com>
2025-10-03 16:26:01 -0600

    Updated dependencies to version C

---
+++ trial_scenario.py
@@ -0,0 +1,12 @@
+
+def find_largest_element(lst):
+    max_element = lst[0]
+    for element in lst:
+        if element > max_element:
+            max_element = element
+    return max_element
+
+# Test the function
+lst = [10, 25, 8, 45, 15]
+largest_element = find_largest_element(lst)
+print(f"The largest element in the list is: {largest_element}")
