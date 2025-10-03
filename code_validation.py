cebcc91f4a52d1e09fd63a79685f9fe0d54a6767
Austin L. Howard <austin.lane.howard@gmail.com>
2025-10-02 18:43:05 -0600

    Updated error messages for better user understanding

---
+++ code_validation.py
@@ -0,0 +1,18 @@
+
+def is_palindrome(num):
+    original_num = num
+    reverse_num = 0
+    while num > 0:
+        digit = num % 10
+        reverse_num = reverse_num * 10 + digit
+        num = num // 10
+    if original_num == reverse_num:
+        return True
+    else:
+        return False
+
+num = int(input("Enter a number: "))
+if is_palindrome(num):
+    print(f"{num} is a palindrome.")
+else:
+    print(f"{num} is not a palindrome.")
