a9bcf2c4a47622c176647fdfc1c21fa5ead5d963
Austin L. Howard <austin.lane.howard@gmail.com>
2025-10-10 18:25:33 -0600

    Streamlined code logic in module T

---
+++ acceptance_probe.py
@@ -0,0 +1,18 @@
+
+import re
+
+def extract_emails(text):
+    email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    emails = re.findall(email_pattern, text)
+    return emails
+
+# Read the text document
+with open('text_document.txt', 'r') as file:
+    text = file.read()
+    
+# Extract email addresses from the text
+emails = extract_emails(text)
+
+# Print the extracted email addresses
+for email in emails:
+    print(email)
