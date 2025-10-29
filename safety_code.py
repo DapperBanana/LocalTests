beddb35963b238478b32a153e9a4c109c8109a73
Austin L. Howard <austin.lane.howard@gmail.com>
2025-10-29 12:04:21 -0600

    Improved logging for better debugging

---
+++ safety_code.py
@@ -0,0 +1,15 @@
+
+import re
+
+def is_valid_email(email):
+    if re.match(r'^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$', email):
+        return True
+    else:
+        return False
+
+email = input("Enter an email address: ")
+
+if is_valid_email(email):
+    print("Valid email address")
+else:
+    print("Invalid email address")
