32efd86d301661d613fc629c2f3b4499b5035e8b
Austin L. Howard <austin.lane.howard@gmail.com>
2025-11-05 12:55:25 -0700

    Merged feature branch N into main

---
+++ preliminary_validation.py
@@ -0,0 +1,34 @@
+
+import heapq
+
+def dijkstra(graph, start):
+    distances = {node: float('infinity') for node in graph}
+    distances[start] = 0
+    pq = [(0, start)]
+    
+    while pq:
+        current_distance, current_node = heapq.heappop(pq)
+        
+        if current_distance > distances[current_node]:
+            continue
+        
+        for neighbor, weight in graph[current_node].items():
+            distance = current_distance + weight
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(pq, (distance, neighbor))
+    
+    return distances
+
+graph = {
+    'A': {'B': 1, 'C': 4},
+    'B': {'A': 1, 'C': 2, 'D': 5},
+    'C': {'A': 4, 'B': 2, 'D': 1},
+    'D': {'B': 5, 'C': 1}
+}
+
+start_node = 'A'
+shortest_distances = dijkstra(graph, start_node)
+
+for node, distance in shortest_distances.items():
+    print(f'Shortest distance from {start_node} to {node} is {distance}')
