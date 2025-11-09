368a8ab03e29822e98ef99faccdba1faf88079be
Austin L. Howard <austin.lane.howard@gmail.com>
2025-11-08 18:25:53 -0700

    Implemented user permission checks in module JJ

---
+++ trial_diagnostic.py
@@ -0,0 +1,10 @@
+
+def reverse_string(input_str):
+    return input_str[::-1]
+
+# Get user input
+user_input = input("Enter a string to be reversed: ")
+
+# Call the function and print the reversed string
+reversed_string = reverse_string(user_input)
+print("Reversed string: ", reversed_string)
