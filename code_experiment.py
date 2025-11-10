581388edc909b8eec99988a4f7473fd9df213584
Austin L. Howard <austin.lane.howard@gmail.com>
2025-11-09 18:38:27 -0700

    Removed unnecessary debug statements

--- code_experiment.py
+++ code_experiment.py
@@ -1,10 +1,25 @@
 
-import json
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
 
-# Open the JSON file
-with open('data.json') as f:
-    data = json.load(f)
+def is_lucas_carmichael(num):
+    if num % 2 == 0:
+        return False
 
-# Print key-value pairs
-for key, value in data.items():
-    print(f"{key}: {value}")
+    for p in range(2, num):
+        if num % p == 0 and is_prime(p):
+            if p % num != 1 and (num // p) % num != 1:
+                return False
+
+    return True
+
+num = int(input("Enter a number: "))
+if is_lucas_carmichael(num):
+    print(num, "is a Lucas-Carmichael number.")
+else:
+    print(num, "is not a Lucas-Carmichael number.")
