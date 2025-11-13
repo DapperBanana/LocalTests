951cdac5742bc12b09113168ef16914395bc7f14
Austin L. Howard <austin.lane.howard@gmail.com>
2025-11-13 12:39:12 -0700

    Refactored code to improve code readability

--- trial_diagnostic.py
+++
@@ -1,8 +0,0 @@
-
-def calculate_sum(n):
-    return n * (n + 1) // 2
-
-n = int(input("Enter a positive integer n: "))
-result = calculate_sum(n)
-
-print(f"The sum of the first {n} natural numbers is: {result}")
