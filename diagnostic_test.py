eddfedab164ec6981f3242e59add50936c2a027c
Austin L. Howard <austin.lane.howard@gmail.com>
2025-11-19 18:55:17 -0700

    Updated dependencies to version C

--- diagnostic_test.py
+++
@@ -1,9 +0,0 @@
-
-def decimal_to_binary(decimal_num):
-    binary_num = bin(decimal_num)[2:]
-    return binary_num
-
-decimal_number = int(input("Enter a decimal number: "))
-binary_number = decimal_to_binary(decimal_number)
-
-print(f"The binary representation of {decimal_number} is: {binary_number}")
