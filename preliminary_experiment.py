e66a06a36be92ba91b1b2e91af4b71a8e285f33f
Austin L. Howard <austin.lane.howard@gmail.com>
2025-12-05 17:03:13 -0700

    Updated project logo and branding

---
+++ preliminary_experiment.py
@@ -0,0 +1,29 @@
+public class Queue<T>
+{
+    private LinkedList<T> _list = new LinkedList<T>();
+
+    public void Enqueue(T item)
+    {
+        _list.AddLast(item);
+    }
+
+    public T Dequeue()
+    {
+        if (_list.Count == 0)
+            throw new InvalidOperationException("Queue is empty");
+        
+        T value = _list.First.Value;
+        _list.RemoveFirst();
+        return value;
+    }
+
+    public T Peek()
+    {
+        if (_list.Count == 0)
+            throw new InvalidOperationException("Queue is empty");
+        
+        return _list.First.Value;
+    }
+
+    public int Count => _list.Count;
+}
