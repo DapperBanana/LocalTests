19ece8f972195d8a2bd9074e5ab3572979589b87
Austin L. Howard <austin.lane.howard@gmail.com>
2025-12-09 17:03:22 -0700

    Fixed memory leaks in feature FF

---
+++ exploratory_trial_run.py
@@ -0,0 +1,27 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the first number:");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the second number:");
+        int num2 = int.Parse(Console.ReadLine());
+
+        int gcd = GCD(num1, num2);
+        Console.WriteLine($"The GCD of {num1} and {num2} is {gcd}");
+    }
+
+    static int GCD(int a, int b)
+    {
+        while (b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
